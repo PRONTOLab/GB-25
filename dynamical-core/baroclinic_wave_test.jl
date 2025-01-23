dd423f85d63b38d54299db16c7d01aa2f2862845
Gregory Wagner <wagner.greg@gmail.com>
2025-01-23 06:49:09 -0500

    Add a few simple scripts

---
+++ dynamical-core/baroclinic_wave_test.jl
@@ -0,0 +1,70 @@
+using Oceananigans
+using Oceananigans.Units: minutes, days
+using Reactant
+
+# Reactant.Ops.DEBUG_MODE[] = true
+
+resolution = 1/4 # degree. eg resolution=1/4 is pronounced "quarter degree"
+arch = GPU() # CPU() to run on CPU
+Nx = Int(resolution * 360)
+Ny = Int(resolution * 160)
+Nz = 100
+Δt = 5minutes
+stop_time = 10Δt #30days
+
+grid = LatitudeLongitudeGrid(arch,
+                             size = (Nx, Ny, Nz),
+                             halo = (7, 7, 7),
+                             longitude = (0, 360),
+                             latitude = (-80, 80),
+                             z = (-1000, 0))
+
+# A configuration for idealized global simulation:
+model = HydrostaticFreeSurfaceModel(; grid,
+                                    momentum_advection = WENOVectorInvariant(),
+                                    tracer_advection = WENO(order=7),
+                                    coriolis = HydrostaticSphericalCoriolis(),
+                                    tracers = :b,
+                                    buoyancy = BuoyancyTracer())
+
+# Baroclinically unstable initial condition:
+N² = 1e-6 # vertical buoyancy gradient
+db = 1e-2 * N² * 1000 / Nz
+M² = 1e-7 # horizontal buoyancy gradient
+φ₀ = 45
+dφ = 5
+
+step(x, a, d) = 1/2 * (tanh((x - a) / d) - 1)
+bᵢ(λ, φ, z) = N² * z + M² * step(φ, φ₀, dφ) + db * randn()
+set!(model, b=bᵢ)
+
+simulation = Simulation(model; Δt, stop_time)
+run!(simulation)
+
+#using CUDA
+#CUDA.@device_code dir="cudajl" run!(simulation)
+
+#=
+# What we want to do with Reactant:
+r_model = Reactant.to_rarray(model)
+r_simulation = Simulation(r_model, Δt=60, stop_iteration=2)
+pop!(r_simulation.callbacks, :nan_checker)
+# @show @code_hlo optimize=:before_kernel run!(r_simulation)
+
+r_run! = @compile sync = true run!(r_simulation)
+r_run!(r_simulation)
+
+Reactant.with_profiler("./notrace4/") do
+    run!(simulation)
+end
+
+Reactant.with_profiler("./retrace4/") do
+    r_run!(r_simulation)
+end
+
+using BenchmarkTools
+
+@btime run!(simulation)
+@btime r_run!(r_simulation)
+=#
+
---
+++ dynamical-core/super_simple_simulation.jl
@@ -0,0 +1,48 @@
+using Oceananigans
+using Reactant
+
+# Reactant.Ops.DEBUG_MODE[] = true
+
+arch = GPU() # CPU() to run on CPU
+Nx, Ny, Nz = (360, 120, 100) # number of cells
+
+grid = LatitudeLongitudeGrid(arch, size=(Nx, Ny, Nz), halo=(7, 7, 7),
+                             longitude=(0, 360), latitude=(-60, 60), z=(-1000, 0))
+
+# One of the implest configurations we might consider:
+model = HydrostaticFreeSurfaceModel(; grid, momentum_advection=WENO())
+
+@assert model.free_surface isa SplitExplicitFreeSurface
+
+uᵢ(x, y, z) = randn()
+set!(model, u=uᵢ, v=uᵢ)
+
+# What we normally do:
+simulation = Simulation(model, Δt=60, stop_iteration=2)
+run!(simulation)
+
+#using CUDA
+#CUDA.@device_code dir="cudajl" run!(simulation)
+
+# What we want to do with Reactant:
+r_model = Reactant.to_rarray(model)
+r_simulation = Simulation(r_model, Δt=60, stop_iteration=2)
+pop!(r_simulation.callbacks, :nan_checker)
+# @show @code_hlo optimize=:before_kernel run!(r_simulation)
+
+r_run! = @compile sync = true run!(r_simulation)
+r_run!(r_simulation)
+
+Reactant.with_profiler("./notrace4/") do
+    run!(simulation)
+end
+
+Reactant.with_profiler("./retrace4/") do
+    r_run!(r_simulation)
+end
+
+using BenchmarkTools
+
+@btime run!(simulation)
+@btime r_run!(r_simulation)
+
