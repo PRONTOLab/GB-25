f1d0c1d0b9b29508a099a7937a57087f78b48e68
Gregory Wagner <wagner.greg@gmail.com>
2025-01-24 15:30:49 -0500

    Update baroclinic wave test

--- oceananigans-dynamical-core/baroclinic_wave_test.jl
+++ oceananigans-dynamical-core/baroclinic_wave_test.jl
@@ -1,16 +1,18 @@
 using Oceananigans
 using Oceananigans.Units: minutes, days
+using Printf
 using Reactant
 
 # Reactant.Ops.DEBUG_MODE[] = true
 
 resolution = 1/4 # degree. eg resolution=1/4 is pronounced "quarter degree"
 arch = GPU() # CPU() to run on CPU
-Nx = Int(resolution * 360)
-Ny = Int(resolution * 160)
+Nx = Int(360 / resolution)
+Ny = Int(160 / resolution)
 Nz = 100
-Δt = 5minutes
-stop_time = 10Δt #30days
+Δt = 10minutes
+# stop_time = 10Δt #30days
+stop_time = 30days
 
 grid = LatitudeLongitudeGrid(arch,
                              size = (Nx, Ny, Nz),
@@ -27,6 +29,9 @@ model = HydrostaticFreeSurfaceModel(; grid,
                                     tracers = :b,
                                     buoyancy = BuoyancyTracer())
 
+@info "Built a model:"
+@show model
+
 # Baroclinically unstable initial condition:
 N² = 1e-6 # vertical buoyancy gradient
 db = 1e-2 * N² * 1000 / Nz
@@ -35,10 +40,32 @@ M² = 1e-7 # horizontal buoyancy gradient
 dφ = 5
 
 step(x, a, d) = 1/2 * (tanh((x - a) / d) - 1)
-bᵢ(λ, φ, z) = N² * z + M² * step(φ, φ₀, dφ) + db * randn()
+bᵢ(λ, φ, z) = N² * z + M² * dφ * step(φ, φ₀, dφ) + db * randn()
 set!(model, b=bᵢ)
 
 simulation = Simulation(model; Δt, stop_time)
+
+stopwatch = Ref(time_ns())
+
+function progress(sim)
+    model = sim.model
+    u, v, w = model.velocities
+    elapsed = 1e-9 * (time_ns() - stopwatch[])
+
+    msg = @sprintf("Iter: %d, time: %s, wall time: %s, maximum|u|: (%.2e, %.2e, %.2e)",
+                   iteration(sim), prettytime(sim), prettytime(elapsed),
+                   maximum(abs, u),
+                   maximum(abs, v),
+                   maximum(abs, w))
+    @info msg
+
+    stopwatch[] = time_ns()
+
+    return nothing
+end
+
+add_callback!(simulation, progress, IterationInterval(100))
+
 run!(simulation)
 
 #using CUDA
