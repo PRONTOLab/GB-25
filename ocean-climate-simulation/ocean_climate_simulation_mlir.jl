b56154aea9f6051cd08a55698a4513446f0288f0
Mosè Giordano <m.giordano@ucl.ac.uk>
2025-02-20 19:18:52 +0000

    DROPME: sprinkle timers to find bottleneck

--- ocean-climate-simulation/ocean_climate_simulation_mlir.jl
+++ ocean-climate-simulation/ocean_climate_simulation_mlir.jl
@@ -7,10 +7,11 @@ ENV["use-reactant"] = true
 include("data_free_ocean_climate_simulation.jl")
 
 Profile.clear()
-@profile unopt = @code_hlo optimize=false run!(simulation)
-open("profile_unopt_ocean_climate_simulation.txt", "w") do s
-    Profile.print(IOContext(s, :displaysize => (48, 1000)))
-end
+# @profile
+unopt = @code_hlo optimize=false run!(simulation)
+# open("profile_unopt_ocean_climate_simulation.txt", "w") do s
+#     Profile.print(IOContext(s, :displaysize => (48, 1000)))
+# end
 
 # Unoptimized HLO
 open("unopt_ocean_climate_simulation.mlir", "w") do io
@@ -19,10 +20,11 @@ end
 
 # Optimized HLO
 Profile.clear()
-@profile opt = @code_hlo optimize=:before_jit run!(simulation)
-open("profile_opt_ocean_climate_simulation.txt", "w") do s
-    Profile.print(IOContext(s, :displaysize => (48, 1000)))
-end
+#@profile
+opt = @code_hlo optimize=:before_jit run!(simulation)
+# open("profile_opt_ocean_climate_simulation.txt", "w") do s
+#     Profile.print(IOContext(s, :displaysize => (48, 1000)))
+# end
 
 open("opt_ocean_climate_simulation.mlir", "w") do io
     write(io, string(opt))
