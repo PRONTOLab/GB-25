03eed0d11a6134e4d2554fc4e6269749993a6654
Mosè Giordano <m.giordano@ucl.ac.uk>
2025-02-20 18:09:38 +0000

    [CI] Profile HLO code generation

--- ocean-climate-simulation/ocean_climate_simulation_mlir.jl
+++ ocean-climate-simulation/ocean_climate_simulation_mlir.jl
@@ -1,10 +1,16 @@
+using Profile
+
 ENV["run"] = false
 ENV["raise"] = true
 ENV["use-reactant"] = true
 
 include("data_free_ocean_climate_simulation.jl")
 
-unopt = @code_hlo optimize=false run!(simulation) 
+Profile.clear()
+@profile unopt = @code_hlo optimize=false run!(simulation)
+open("profile_unopt_ocean_climate_simulation.txt", "w") do s
+    Profile.print(IOContext(s, :displaysize => (48, 1000)))
+end
 
 # Unoptimized HLO
 open("unopt_ocean_climate_simulation.mlir", "w") do io
@@ -12,9 +18,12 @@ open("unopt_ocean_climate_simulation.mlir", "w") do io
 end
 
 # Optimized HLO
-opt = @code_hlo optimize=:before_jit run!(simulation) 
+Profile.clear()
+@profile opt = @code_hlo optimize=:before_jit run!(simulation)
+open("profile_opt_ocean_climate_simulation.txt", "w") do s
+    Profile.print(IOContext(s, :displaysize => (48, 1000)))
+end
 
 open("opt_ocean_climate_simulation.mlir", "w") do io
     write(io, string(opt))
 end
-
