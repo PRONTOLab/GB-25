c96432583d03a511bee1a85a5df8b189d1ddbf7d
Avik Pal <avikpal@mit.edu>
2025-02-28 18:27:00 -0600

    feat: use the new sharding

--- oceananigans-dynamical-core/super_simple_simulation_sharded.jl
+++ oceananigans-dynamical-core/super_simple_simulation_sharded.jl
@@ -2,6 +2,9 @@ using Oceananigans
 using Reactant
 using Libdl
 using Reactant_jll
+using Setfield
+
+include("sharding_helpers.jl")
 
 Reactant.Ops.DEBUG_MODE[] = true
 ENV["JULIA_DEBUG"] = "Reactant_jll"
@@ -16,32 +19,38 @@ reactant_devices = all_reactant_devices[1:4]
 mesh = Sharding.Mesh(reshape(reactant_devices, 2, 2), (:x, :y))
 
 # Set up a very simple Oceananigans simulation:
-# arch = Oceananigans.Architectures.ReactantState() # CPU() to run on CPU
-arch = Oceananigans.Architectures.ReactantState(
-    Sharding.NamedSharding(mesh, (:x, :y))
-)
+arch = CPU()
 
-grid = LatitudeLongitudeGrid(arch,
-                             size = (360, 160, 100), # number of cells, can certainly increase
-                             halo = (7, 7, 7),
-                             longitude = (0, 360),
-                             latitude = (-80, 80),
-                             z = (-1000, 0))
+core_sharding = TreeSharding(Sharding.DimsSharding(mesh, (2, 2), (:x, :y)))
+
+grid = LatitudeLongitudeGrid(
+    arch,
+    size=(360, 160, 100), # number of cells, can certainly increase
+    halo=(7, 7, 7),
+    longitude=(0, 360),
+    latitude=(-80, 80),
+    z=(-1000, 0)
+)
 
 model = HydrostaticFreeSurfaceModel(; grid, momentum_advection=WENOVectorInvariant())
 @assert model.free_surface isa SplitExplicitFreeSurface
 
+model_ra = Reactant.to_rarray(model) # without sharding
+@set! model_ra.velocities.u = Reactant.to_rarray(model.velocities.u; sharding=core_sharding);
+@set! model_ra.velocities.v = Reactant.to_rarray(model.velocities.v; sharding=core_sharding);
+@set! model_ra.velocities.w = Reactant.to_rarray(model.velocities.w; sharding=core_sharding);
+
 uᵢ(x, y, z) = randn()
 set!(model, u=uᵢ, v=uᵢ)
 
-simulation = Simulation(model, Δt=60, stop_iteration=10, verbose=false)
+simulation = Simulation(model_ra, Δt=60, stop_iteration=10, verbose=false)
 pop!(simulation.callbacks, :nan_checker)
 
-if arch isa Oceananigans.Architectures.ReactantState
-    _run! = @compile sync=true run!(simulation)
-else
-    _run! = run!
-end
+# if arch isa Oceananigans.Architectures.ReactantState
+_run! = @compile sync = true run!(simulation)
+# else
+#     _run! = run!
+# end
 
 _run!(simulation)
 
