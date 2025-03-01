830b801fbc682704260dd946a12b5d90cd2c76e5
Avik Pal <avikpal@mit.edu>
2025-02-28 21:32:22 -0600

    fix: shard only velocities

--- oceananigans-dynamical-core/super_simple_simulation_sharded.jl
+++ oceananigans-dynamical-core/super_simple_simulation_sharded.jl
@@ -19,12 +19,9 @@ reactant_devices = all_reactant_devices[1:4]
 mesh = Sharding.Mesh(reshape(reactant_devices, 2, 2), (:x, :y))
 
 # Set up a very simple Oceananigans simulation:
-arch = CPU()
+sharding = TreeSharding(Sharding.DimsSharding(mesh, (2, 2), (:x, :y)))
 
-core_sharding = TreeSharding(Sharding.DimsSharding(mesh, (2, 2), (:x, :y)))
-
-grid = LatitudeLongitudeGrid(
-    arch,
+grid_kwargs = (;
     size=(360, 160, 100), # number of cells, can certainly increase
     halo=(7, 7, 7),
     longitude=(0, 360),
@@ -32,13 +29,20 @@ grid = LatitudeLongitudeGrid(
     z=(-1000, 0)
 )
 
+grid = LatitudeLongitudeGrid(Oceananigans.Architectures.ReactantState(); grid_kwargs...)
+
+grid_cpu = LatitudeLongitudeGrid(CPU(); grid_kwargs...)
+
 model = HydrostaticFreeSurfaceModel(; grid, momentum_advection=WENOVectorInvariant())
 @assert model.free_surface isa SplitExplicitFreeSurface
 
-model_ra = Reactant.to_rarray(model) # without sharding
-@set! model_ra.velocities.u = Reactant.to_rarray(model.velocities.u; sharding=core_sharding);
-@set! model_ra.velocities.v = Reactant.to_rarray(model.velocities.v; sharding=core_sharding);
-@set! model_ra.velocities.w = Reactant.to_rarray(model.velocities.w; sharding=core_sharding);
+model_cpu = HydrostaticFreeSurfaceModel(;
+    grid=grid_cpu, momentum_advection=WENOVectorInvariant()
+)
+
+@set! model.velocities.u = Reactant.to_rarray(model_cpu.velocities.u; sharding);
+@set! model.velocities.v = Reactant.to_rarray(model_cpu.velocities.v; sharding);
+@set! model.velocities.w = Reactant.to_rarray(model_cpu.velocities.w; sharding);
 
 uᵢ(x, y, z) = randn()
 set!(model, u=uᵢ, v=uᵢ)
