368bb77b28b28776a1d463994cf37c93c72c5b46
Mosè Giordano <mose@gnu.org>
2025-03-07 14:45:15 -0600

    Restore precompilation (with a fix) to show new segfault

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,10 +1,8 @@
 using PrecompileTools: @setup_workload, @compile_workload
 
-# Julia v1.11.3 caches bad code: <https://github.com/EnzymeAD/Reactant.jl/issues/614>.
-if VERSION != v"1.11.3"
-    @setup_workload begin
-        @compile_workload begin
-            data_free_ocean_climate_simulation_init()
-        end
+@setup_workload begin
+    @compile_workload begin
+        data_free_ocean_climate_simulation_init()
     end
+    Reactant.clear_oc_cache()
 end
