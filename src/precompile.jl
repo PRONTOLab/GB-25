1a70085ad0faec06594e4601d2cbdb881fccf3ff
Mosè Giordano <mose@gnu.org>
2025-03-07 15:46:27 -0600

    Start crafting reproducer for new segfault

--- src/precompile.jl
+++ src/precompile.jl
@@ -2,7 +2,7 @@ using PrecompileTools: @setup_workload, @compile_workload
 
 @setup_workload begin
     @compile_workload begin
-        data_free_ocean_climate_simulation_init()
+        _grid(Architectures.ReactantState())
     end
     Reactant.clear_oc_cache()
 end
