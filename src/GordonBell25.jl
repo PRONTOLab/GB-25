d7fc20880b687fe8b927023cf8872089a2a2ed40
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-03-07 02:55:08 +0000

    Temporarily disable precompilation

--- src/GordonBell25.jl
+++ src/GordonBell25.jl
@@ -1,6 +1,8 @@
 module GordonBell25
 
 include("data_free_ocean_climate_simulation.jl")
-include("precompile.jl")
+# Temporarily disable precompilation until
+# https://github.com/PRONTOLab/GB-25/issues/30 is resolved.
+# include("precompile.jl")
 
 end # module
