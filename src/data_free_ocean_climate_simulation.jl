ffebbf6898769b6082c26061b7baa326612d3aca
Mosè Giordano <mose@gnu.org>
2025-03-07 10:11:40 -0600

    Great simplification

--- src/data_free_ocean_climate_simulation.jl
+++ src/data_free_ocean_climate_simulation.jl
@@ -1,29 +1,8 @@
-using Oceananigans
-using Oceananigans.Units
-using Oceananigans.Architectures: Architectures
-using Oceananigans.Solvers.KernelAbstractions
-using Reactant
+using KernelAbstractions, Reactant, Oceananigans
 
-using ClimaOcean
-using OrthogonalSphericalShellGrids: TripolarGrid
-
-@kernel function _compute_numerical_bottom_height!(_, _, _)
-    nothing
-end
-
-function MyImmersedBoundaryGrid(arch, grid)
-    Oceananigans.Utils.launch!(arch, grid, :xy, _compute_numerical_bottom_height!, nothing, nothing, nothing)
-    return nothing
-end
-
-function _grid(arch::Architectures.AbstractArchitecture)
-    resolution = 2
-    Nx = convert(Int, 360 / resolution)
-    Ny = convert(Int, 170 / resolution)
-    Nz = 20
-    Δt = 1minutes
-    z_faces = exponential_z_faces(; Nz, depth=4000, h=30)
-    underlying_grid = TripolarGrid(arch; size=(Nx, Ny, Nz), halo=(7, 7, 7), z=z_faces)
-
-    return MyImmersedBoundaryGrid(arch, underlying_grid)
+@kernel kernel!() = nothing
+function launch_kernel()
+    dev = KernelAbstractions.get_backend(Reactant.to_rarray(Float32[]))
+    workgroup, worksize = (16, 16), (180, 85)
+    kernel!(dev, workgroup, worksize)()
 end
--- src/precompile.jl
+++ src/precompile.jl
@@ -1,11 +1,5 @@
-using PrecompileTools: @setup_workload, @compile_workload
-using Oceananigans.Architectures
+using PrecompileTools: @compile_workload
 
-# Julia v1.11.3 caches bad code: <https://github.com/EnzymeAD/Reactant.jl/issues/614>.
-if true  # VERSION != v"1.11.3"
-    @setup_workload begin
-        @compile_workload begin
-            _grid(Architectures.ReactantState())
-        end
-    end
+@compile_workload begin
+    launch_kernel()
 end
