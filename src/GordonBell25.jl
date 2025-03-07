6869c868c6a577ea30494289a8aeea3359cd906c
Mosè Giordano <mose@gnu.org>
2025-03-07 07:09:43 -0600

    Craft segfault reproducer

--- src/GordonBell25.jl
+++ src/GordonBell25.jl
@@ -1,8 +1,6 @@
 module GordonBell25
 
 include("data_free_ocean_climate_simulation.jl")
-# Temporarily disable precompilation until
-# https://github.com/PRONTOLab/GB-25/issues/30 is resolved.
-# include("precompile.jl")
+include("precompile.jl")
 
 end # module
--- src/precompile.jl
+++ src/precompile.jl
@@ -1,10 +1,11 @@
 using PrecompileTools: @setup_workload, @compile_workload
+using Oceananigans.Architectures
 
 # Julia v1.11.3 caches bad code: <https://github.com/EnzymeAD/Reactant.jl/issues/614>.
-if VERSION != v"1.11.3"
+if true  # VERSION != v"1.11.3"
     @setup_workload begin
         @compile_workload begin
-            data_free_ocean_climate_simulation_init()
+            _grid(Architectures.ReactantState())
         end
     end
 end
