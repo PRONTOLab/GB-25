421e310bd692c8a14dd0dcd83bc701d9fd28cf0c
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-03-08 21:21:34 +0000

    Avoid conversion of (potentially large) MLIR modules to strings

--- ocean-climate-simulation/ocean_climate_simulation_mlir.jl
+++ ocean-climate-simulation/ocean_climate_simulation_mlir.jl
@@ -11,12 +11,12 @@ unopt = @code_hlo optimize=false raise=true run!(simulation)
 
 # Unoptimized HLO
 open("unopt_ocean_climate_simulation.mlir", "w") do io
-    write(io, string(unopt))
+    show(io, unopt)
 end
 
 # Optimized HLO
 opt = @code_hlo optimize=:before_jit raise=true run!(simulation)
 
 open("opt_ocean_climate_simulation.mlir", "w") do io
-    write(io, string(opt))
+    show(io, opt)
 end
