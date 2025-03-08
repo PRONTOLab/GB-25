c194168b1b9958a1428bc0ef3854bcbe20994648
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-03-08 19:20:39 +0000

    Reorganise `data_free_ocean_climate_simulation.jl` to use functions (#33)

--- ocean-climate-simulation/ocean_climate_simulation_mlir.jl
+++ ocean-climate-simulation/ocean_climate_simulation_mlir.jl
@@ -1,7 +1,11 @@
-ENV["run"] = false
-ENV["use-reactant"] = true
+using Reactant
+using Oceananigans.Architectures: Architectures
 
 include("data_free_ocean_climate_simulation.jl")
+PROFILE[] = true
+simulation = data_free_ocean_climate_simulation_init(Architectures.ReactantState())
+
+GC.gc(true); GC.gc(false); GC.gc(true)
 
 unopt = @code_hlo optimize=false raise=true run!(simulation)
 
