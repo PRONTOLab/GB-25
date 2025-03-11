a94591d56b49f41c1c5b6c5aa4b9ae56e27769ed
Mosè Giordano <m.giordano@ucl.ac.uk>
2025-03-11 13:09:27 +0000

    Actually define `Ninner`

--- ocean-climate-simulation/ocean_climate_simulation_mlir.jl
+++ ocean-climate-simulation/ocean_climate_simulation_mlir.jl
@@ -10,7 +10,7 @@ model = data_free_ocean_climate_model_init(Architectures.ReactantState())
 
 GC.gc(true); GC.gc(false); GC.gc(true)
 
-function loop!(model)
+function loop!(model, Ninner)
     Δt = 1200 # 20 minutes
     Oceananigans.TimeSteppers.first_time_step!(model, Δt)
     @trace for _ = 2:Ninner
@@ -21,11 +21,11 @@ end
 
 # Unoptimized HLO
 @info "Compiling unoptimised kernel..."
-unopt = @code_hlo optimize=false raise=true loop!(model)
+unopt = @code_hlo optimize=false raise=true loop!(model, 10)
 
 # Optimized HLO
 @info "Compiling optimised kernel..."
-opt = @code_hlo optimize=:before_jit raise=true loop!(model)
+opt = @code_hlo optimize=:before_jit raise=true loop!(model, 10)
 
 for debug in (true, false)
     # Unoptimized HLO
