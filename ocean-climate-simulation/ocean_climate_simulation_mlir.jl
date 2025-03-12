db557405ba188ea7203883a36d3566fa100c9fdc
Paul Berg <naydex.mc+github@gmail.com>
2025-03-12 17:28:28 +0100

    Emit debug info in mlir

--- ocean-climate-simulation/ocean_climate_simulation_mlir.jl
+++ ocean-climate-simulation/ocean_climate_simulation_mlir.jl
@@ -11,12 +11,12 @@ unopt = @code_hlo optimize=false raise=true run!(simulation)
 
 # Unoptimized HLO
 open("unopt_ocean_climate_simulation.mlir", "w") do io
-    show(io, unopt)
+    show(IOContext(io, :debug => true), unopt)
 end
 
 # Optimized HLO
 opt = @code_hlo optimize=:before_jit raise=true run!(simulation)
 
 open("opt_ocean_climate_simulation.mlir", "w") do io
-    show(io, opt)
+    show(IOContext(io, :debug => true), opt)
 end
