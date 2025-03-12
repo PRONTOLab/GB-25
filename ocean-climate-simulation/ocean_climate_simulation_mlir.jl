5e3135c109ef9ba85470c0d855dccc43448f5ca1
Mosè Giordano <mose@gnu.org>
2025-03-11 19:24:10 -0500

    Start crafting reproducer for JIT session errors with Julia v1.10

--- ocean-climate-simulation/ocean_climate_simulation_mlir.jl
+++ ocean-climate-simulation/ocean_climate_simulation_mlir.jl
@@ -1,23 +1,30 @@
+using Dates
+@info "loading GB25" now(UTC)
 using GordonBell25: data_free_ocean_climate_simulation_init, PROFILE
+@info "loading Reactant" now(UTC)
 using Reactant: @code_hlo
+@info "loading Reactant" now(UTC)
 using Oceananigans: run!
 using Oceananigans.Architectures: ReactantState
 
-PROFILE[] = true
+# PROFILE[] = true
+@info "init" now(UTC)
 simulation = data_free_ocean_climate_simulation_init(ReactantState())
 
-GC.gc(true); GC.gc(false); GC.gc(true)
+@info "all done" now(UTC)
 
-unopt = @code_hlo optimize=false raise=true run!(simulation)
+# GC.gc(true); GC.gc(false); GC.gc(true)
 
-# Unoptimized HLO
-open("unopt_ocean_climate_simulation.mlir", "w") do io
-    show(io, unopt)
-end
+# unopt = @code_hlo optimize=false raise=true run!(simulation)
 
-# Optimized HLO
-opt = @code_hlo optimize=:before_jit raise=true run!(simulation)
+# # Unoptimized HLO
+# open("unopt_ocean_climate_simulation.mlir", "w") do io
+#     show(io, unopt)
+# end
 
-open("opt_ocean_climate_simulation.mlir", "w") do io
-    show(io, opt)
-end
+# # Optimized HLO
+# opt = @code_hlo optimize=:before_jit raise=true run!(simulation)
+
+# open("opt_ocean_climate_simulation.mlir", "w") do io
+#     show(io, opt)
+# end
--- src/data_free_ocean_climate_simulation.jl
+++ src/data_free_ocean_climate_simulation.jl
@@ -4,59 +4,8 @@ using Oceananigans.Architectures: Architectures
 using Reactant
 
 using ClimaOcean
-using ClimaOcean.OceanSeaIceModels.InterfaceComputations: FixedIterations, ComponentInterfaces
+using ClimaOcean.OceanSeaIceModels.InterfaceComputations: FixedIterations, ComponentInterfaces, CoefficientBasedFluxes, BulkTemperature, default_ao_specific_humidity, default_ai_temperature, reference_density, heat_capacity, DegreesCelsius, thermodynamics_parameters, atmosphere_ocean_interface, sea_ice_ocean_interface, atmosphere_sea_ice_interface, SeaIceSimulation, surface_flux, StateExchanger, atmosphere_exchanger
 using OrthogonalSphericalShellGrids: TripolarGrid
-
-using CFTime
-using Dates
-using Printf
-using Profile
-
-const PROFILE = Ref(false)
-
-macro gbprofile(name::String, expr::Expr)
-    return quote
-        if $(PROFILE)[]
-            $(Profile.clear)()
-            $(Profile.init)(; delay=0.1)
-            out = $(Profile).@profile $(esc(expr))
-            open(string("profile_", $(esc(name)), ".txt"), "w") do s
-                println(s, "# Showing profile of")
-                println(s, "#     ", $(string(expr)))
-                println(s, "# at ", $(string(__source__)))
-                $(Profile.print)(IOContext(s, :displaysize => (48, 1000)))
-            end
-            $(Profile.clear)()
-            out
-        else
-            $(esc(expr))
-        end
-    end
-end
-
-# Utility for printing progress to the terminal
-const wall_time = Ref(UInt64(0))
-
-function progress(sim)
-    ocean = sim.model.ocean
-    u, v, w = ocean.model.velocities
-    T = ocean.model.tracers.T
-    Tmax = maximum(interior(T))
-    Tmin = minimum(interior(T))
-    umax = (maximum(abs, interior(u)), maximum(abs, interior(v)), maximum(abs, interior(w)))
-    step_time = 1e-9 * (time_ns() - wall_time[])
-
-    msg = @sprintf("Time: %s, n: %d, Δt: %s, max|u|: (%.2e, %.2e, %.2e) m s⁻¹, \
-                   extrema(T): (%.2f, %.2f) ᵒC, wall time: %s \n",
-                   prettytime(sim), iteration(sim), prettytime(sim.Δt),
-                   umax..., Tmax, Tmin, prettytime(step_time))
-
-    ClimaOcean.@root @info(msg)
-
-    wall_time[] = time_ns()
-
-    return nothing
-end
 
 function mtn₁(λ, φ)
     λ₁ = 70
@@ -73,27 +22,6 @@ function mtn₂(λ, φ)
     return exp(-((λ - λ₂)^2 + (φ - φ₂)^2) / 2dφ^2)
 end
 
-# Simple initial condition for producing pretty pictures
-function smooth_step(φ)
-    φ₀ = 40
-    dφ = 5
-    return (1 - tanh((abs(φ) - φ₀) / dφ)) / 2
-end
-
-function Tᵢ(λ, φ, z)
-    dTdz = 1e-3
-    return (30 + dTdz * z) * smooth_step(φ) + rand()
-end
-
-function Sᵢ(λ, φ, z)
-    dSdz = - 5e-3
-    return dSdz * z + rand()
-end
-
-zonal_wind(λ, φ) = 4 * sind(2φ)^2 - 2 * exp(-(abs(φ) - 12)^2 / 72)
-sunlight(λ, φ) = -200 - 600 * cosd(φ)^2
-Tatm(λ, φ, z=0) = 30 * cosd(φ)
-
 function gaussian_islands_tripolar_grid(arch::Architectures.AbstractArchitecture, resolution, Nx, Ny, Nz)
     # Time step. This must be decreased as resolution is decreased.
     Δt = 1minutes
@@ -108,8 +36,23 @@ function gaussian_islands_tripolar_grid(arch::Architectures.AbstractArchitecture
     h = -zb + 100
     gaussian_islands(λ, φ) = zb + h * (mtn₁(λ, φ) + mtn₂(λ, φ))
 
-    return @gbprofile "ImmersedBoundaryGrid" ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(gaussian_islands);
-                                                                  active_cells_map=false)
+    return ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(gaussian_islands);
+                                active_cells_map=false)
+end
+
+function MyStateExchanger(exchange_grid, atmosphere)
+    exchange_atmosphere_state = (u  = Field{Center, Center, Nothing}(exchange_grid),
+                                 v  = Field{Center, Center, Nothing}(exchange_grid),
+                                 T  = Field{Center, Center, Nothing}(exchange_grid),
+                                 q  = Field{Center, Center, Nothing}(exchange_grid),
+                                 p  = Field{Center, Center, Nothing}(exchange_grid),
+                                 Qs = Field{Center, Center, Nothing}(exchange_grid),
+                                 Qℓ = Field{Center, Center, Nothing}(exchange_grid),
+                                 Mp = Field{Center, Center, Nothing}(exchange_grid))
+
+    exchanger = atmosphere_exchanger(atmosphere, exchange_grid)
+
+    return StateExchanger(exchange_grid, exchange_atmosphere_state, exchanger)
 end
 
 function data_free_ocean_climate_simulation_init(
@@ -123,16 +66,7 @@ function data_free_ocean_climate_simulation_init(
     )
 
     grid = gaussian_islands_tripolar_grid(arch, resolution, Nx, Ny, Nz)
-
-    # See visualize_ocean_climate_simulation.jl for information about how to
-    # visualize the results of this run.
-    ocean = @gbprofile "ocean_simulation" ocean_simulation(grid)
-
-    @gbprofile "set_ocean_model" set!(ocean.model, T=Tᵢ, S=Sᵢ)
-
-    # Set up an atmosphere
     atmos_times = range(0, 1days, length=24)
-
     atmos_grid = LatitudeLongitudeGrid(arch,
                                        size = (360, 180),
                                        longitude = (0, 360),
@@ -140,56 +74,5 @@ function data_free_ocean_climate_simulation_init(
                                        topology = (Periodic, Bounded, Flat))
 
     atmosphere = PrescribedAtmosphere(atmos_grid, atmos_times)
-
-    Ta = Field{Center, Center, Nothing}(atmos_grid)
-    ua = Field{Center, Center, Nothing}(atmos_grid)
-    Qs = Field{Center, Center, Nothing}(atmos_grid)
-
-    set!(Ta, Tatm)
-    set!(ua, zonal_wind)
-    set!(Qs, sunlight)
-
-    parent(atmosphere.tracers.T) .= parent(Ta) .+ 273.15
-    parent(atmosphere.velocities.u) .= parent(ua)
-    parent(atmosphere.tracers.q) .= 0
-    parent(atmosphere.downwelling_radiation.shortwave) .= parent(Qs)
-
-    # Atmospheric model
-    radiation = Radiation(arch)
-
-    # Coupled model and simulation
-    solver_stop_criteria = FixedIterations(5) # note: more iterations = more accurate
-    atmosphere_ocean_flux_formulation = SimilarityTheoryFluxes(; solver_stop_criteria)
-    interfaces = ComponentInterfaces(atmosphere, ocean; radiation, atmosphere_ocean_flux_formulation)
-    coupled_model = @gbprofile "OceanSeaIceModel" OceanSeaIceModel(ocean; atmosphere, radiation, interfaces)
-    simulation = @gbprofile "Simulation" Simulation(coupled_model; Δt=20minutes, stop_iteration=40)
-    pop!(simulation.callbacks, :nan_checker)
-
-    wall_time[] = time_ns()
-
-    if !(arch isa Architectures.ReactantState)
-        add_callback!(simulation, progress, IterationInterval(10))
-    end
-
-    # Output
-    prefix = if arch isa Distributed
-        "ocean_climate_simulation_rank$(arch.local_rank)"
-    else
-        "ocean_climate_simulation_serial"
-    end
-
-    Nz = size(grid, 3)
-    outputs = merge(ocean.model.velocities, ocean.model.tracers)
-    if !(arch isa Architectures.ReactantState)
-        surface_writer = JLD2OutputWriter(ocean.model, outputs,
-        				  filename = prefix * "_surface.jld2",
-        				  indices = (:, :, Nz),
-        				  schedule = TimeInterval(3days),
-        				  overwrite_existing = true)
-
-        simulation.output_writers[:surface] = surface_writer
-    end
-
-    return simulation
-
+    MyStateExchanger(grid, atmosphere)
 end # data_free_ocean_climate_simulation_init
