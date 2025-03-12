b0f78c610ee3ac23678894b0ce765c5e2ae6ce9d
Mosè Giordano <mose@gnu.org>
2025-03-12 23:03:06 +0000

    Add script for running simulation

---
+++ ocean-climate-simulation/ocean_climate_simulation_run.jl
@@ -0,0 +1,12 @@
+using GordonBell25: data_free_ocean_climate_simulation_init
+using Oceananigans: run!
+using Oceananigans.Architectures: ReactantState
+using Reactant
+
+simulation = data_free_ocean_climate_simulation_init(ReactantState())
+
+GC.gc(true); GC.gc(false); GC.gc(true)
+
+Reactant.with_profiler("./") do
+    @jit run!(simulation)
+end
