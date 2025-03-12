53cfecc7d067d53fc396cdcbfd36fdfa8815a932
Paul Berg <naydex.mc+github@gmail.com>
2025-03-12 20:15:04 +0100

    Generate a non-debug mlir dump

--- ocean-climate-simulation/ocean_climate_simulation_mlir.jl
+++ ocean-climate-simulation/ocean_climate_simulation_mlir.jl
@@ -9,14 +9,16 @@ GC.gc(true); GC.gc(false); GC.gc(true)
 
 unopt = @code_hlo optimize=false raise=true run!(simulation)
 
-# Unoptimized HLO
-open("unopt_ocean_climate_simulation.mlir", "w") do io
-    show(IOContext(io, :debug => true), unopt)
-end
-
 # Optimized HLO
 opt = @code_hlo optimize=:before_jit raise=true run!(simulation)
 
-open("opt_ocean_climate_simulation.mlir", "w") do io
-    show(IOContext(io, :debug => true), opt)
+for debug in (true, false)
+    # Unoptimized HLO
+    open("unopt_ocean_climate_simulation$(debug ? "_debug" : "").mlir", "w") do io
+        show(IOContext(io, :debug => debug), unopt)
+    end
+
+    open("opt_ocean_climate_simulation$(debug ? "_debug" : "").mlir", "w") do io
+        show(IOContext(io, :debug => debug), opt)
+    end
 end
