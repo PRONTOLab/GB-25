fcf85d3ffa3cf112f9d4fa9cb2f1563c2561c55b
William S. Moses <gh@wsmoses.com>
2025-03-13 16:45:24 -0400

    fix

--- src/data_free_ocean_climate_simulation.jl
+++ src/data_free_ocean_climate_simulation.jl
@@ -29,8 +29,28 @@ Base.@nospecializeinfer function Reactant.traced_type_inner(
     G2 = Reactant.traced_type_inner(G, seen, mode, track_numbers, sharding, runtime)
     C2 = Reactant.traced_type_inner(C, seen, mode, track_numbers, sharding, runtime)
     M2 = Reactant.traced_type_inner(M, seen, mode, track_numbers, sharding, runtime)
-    T2 = eltype(O)
+    T2 = eltype(O2)
     return Oceananigans.AbstractOperations.ConditionalOperation{LX2, LY2, LZ2, O2, F2, G2, C2, M2, T2}
+end
+
+# https://github.com/CliMA/Oceananigans.jl/blob/da9959f3e5d8ee7cf2fb42b74ecc892874ec1687/src/AbstractOperations/kernel_function_operation.jl#L3
+# struct KernelFunctionOperation{LX, LY, LZ, G, T, K, D} <: AbstractOperation{LX, LY, LZ, G, T}
+Base.@nospecializeinfer function Reactant.traced_type_inner(
+        @nospecialize(OA::Type{Oceananigans.AbstractOperations.KernelFunctionOperation{LX, LY, LZ, G, T, K, D}}),
+    seen,
+    mode::Reactant.TraceMode,
+    @nospecialize(track_numbers::Type),
+    @nospecialize(sharding),
+    @nospecialize(runtime)
+) where {LX, LY, LZ, G, T, K, D}
+    LX2 = Reactant.traced_type_inner(LX, seen, mode, track_numbers, sharding, runtime)
+    LY2 = Reactant.traced_type_inner(LY, seen, mode, track_numbers, sharding, runtime)
+    LZ2 = Reactant.traced_type_inner(LZ, seen, mode, track_numbers, sharding, runtime)
+    G2 = Reactant.traced_type_inner(G, seen, mode, track_numbers, sharding, runtime)
+    K2 = Reactant.traced_type_inner(K, seen, mode, track_numbers, sharding, runtime)
+    D2 = Reactant.traced_type_inner(D, seen, mode, track_numbers, sharding, runtime)
+    T2 = eltype(G2)
+    return Oceananigans.AbstractOperations.KernelFunctionOperation{LX2, LY2, LZ2, G2, T2, K2, D2}
 end
 
 const PROFILE = Ref(false)
@@ -158,7 +178,7 @@ function data_free_ocean_climate_simulation_init(
     Δt=30seconds
     ocean = @gbprofile "ocean_simulation" ocean_simulation(grid;
                                                            Δt,
-                                                           free_surce=ClimaOcean.OceanSimulations.default_free_surface(grid, fixed_Δt=Δt)
+                                                           free_surface=ClimaOcean.OceanSimulations.default_free_surface(grid, fixed_Δt=Δt)
                                                           )
 
     @gbprofile "set_ocean_model" set!(ocean.model, T=Tᵢ, S=Sᵢ)
