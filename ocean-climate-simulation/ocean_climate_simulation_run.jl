f58c8d6497469a1b9c13606b733468e3a14637d2
Mosè Giordano <mose@gnu.org>
2025-03-13 22:15:39 +0000

    Disable sroa attributor

--- ocean-climate-simulation/ocean_climate_simulation_run.jl
+++ ocean-climate-simulation/ocean_climate_simulation_run.jl
@@ -2,7 +2,7 @@ using GordonBell25: data_free_ocean_climate_model_init
 using Oceananigans.Architectures: ReactantState
 using Reactant
 
-# Reactant.Compiler.SROA_ATTRIBUTOR[] = false
+Reactant.Compiler.SROA_ATTRIBUTOR[] = false
 
 include("common.jl")
 
@@ -18,4 +18,4 @@ rloop! = @compile raise=true loop!(model, 2)
 Reactant.with_profiler("./") do
     rloop!(model, 2)
 end
-@info "done!"
+@info "Done!"
