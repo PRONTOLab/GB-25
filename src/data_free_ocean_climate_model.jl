298e4da489f47b709d4f9e559b5c5db8581d0e54
Mosè Giordano <m.giordano@ucl.ac.uk>
2025-03-14 18:26:03 +0000

    Remove code not needed anymore

--- src/data_free_ocean_climate_model.jl
+++ src/data_free_ocean_climate_model.jl
@@ -12,47 +12,6 @@ using Printf
 using Profile
 using Serialization
 
-# https://github.com/CliMA/Oceananigans.jl/blob/da9959f3e5d8ee7cf2fb42b74ecc892874ec1687/src/AbstractOperations/conditional_operations.jl#L8
-Base.@nospecializeinfer function Reactant.traced_type_inner(
-    @nospecialize(OA::Type{Oceananigans.AbstractOperations.ConditionalOperation{LX, LY, LZ, O, F, G, C, M, T}}),
-    seen,
-    mode::Reactant.TraceMode,
-    @nospecialize(track_numbers::Type),
-    @nospecialize(sharding),
-    @nospecialize(runtime)
-) where {LX, LY, LZ, O, F, G, C, M, T}
-    LX2 = Reactant.traced_type_inner(LX, seen, mode, track_numbers, sharding, runtime)
-    LY2 = Reactant.traced_type_inner(LY, seen, mode, track_numbers, sharding, runtime)
-    LZ2 = Reactant.traced_type_inner(LZ, seen, mode, track_numbers, sharding, runtime)
-    O2 = Reactant.traced_type_inner(O, seen, mode, track_numbers, sharding, runtime)
-    F2 = Reactant.traced_type_inner(F, seen, mode, track_numbers, sharding, runtime)
-    G2 = Reactant.traced_type_inner(G, seen, mode, track_numbers, sharding, runtime)
-    C2 = Reactant.traced_type_inner(C, seen, mode, track_numbers, sharding, runtime)
-    M2 = Reactant.traced_type_inner(M, seen, mode, track_numbers, sharding, runtime)
-    T2 = eltype(O2)
-    return Oceananigans.AbstractOperations.ConditionalOperation{LX2, LY2, LZ2, O2, F2, G2, C2, M2, T2}
-end
-
-# https://github.com/CliMA/Oceananigans.jl/blob/da9959f3e5d8ee7cf2fb42b74ecc892874ec1687/src/AbstractOperations/kernel_function_operation.jl#L3
-# struct KernelFunctionOperation{LX, LY, LZ, G, T, K, D} <: AbstractOperation{LX, LY, LZ, G, T}
-Base.@nospecializeinfer function Reactant.traced_type_inner(
-        @nospecialize(OA::Type{Oceananigans.AbstractOperations.KernelFunctionOperation{LX, LY, LZ, G, T, K, D}}),
-    seen,
-    mode::Reactant.TraceMode,
-    @nospecialize(track_numbers::Type),
-    @nospecialize(sharding),
-    @nospecialize(runtime)
-) where {LX, LY, LZ, G, T, K, D}
-    LX2 = Reactant.traced_type_inner(LX, seen, mode, track_numbers, sharding, runtime)
-    LY2 = Reactant.traced_type_inner(LY, seen, mode, track_numbers, sharding, runtime)
-    LZ2 = Reactant.traced_type_inner(LZ, seen, mode, track_numbers, sharding, runtime)
-    G2 = Reactant.traced_type_inner(G, seen, mode, track_numbers, sharding, runtime)
-    K2 = Reactant.traced_type_inner(K, seen, mode, track_numbers, sharding, runtime)
-    D2 = Reactant.traced_type_inner(D, seen, mode, track_numbers, sharding, runtime)
-    T2 = eltype(G2)
-    return Oceananigans.AbstractOperations.KernelFunctionOperation{LX2, LY2, LZ2, G2, T2, K2, D2}
-end
-
 const PROFILE = Ref(false)
 
 macro gbprofile(name::String, expr::Expr)
@@ -146,7 +105,6 @@ function data_free_ocean_climate_model_init(
     resolution::Real = 2, # 1/4 for quarter degree
     # Vertical resolution
     Nz::Int = 20, # eventually we want to increase this to between 100-600
-    output::Bool = false
     )
 
     grid = gaussian_islands_tripolar_grid(arch, resolution, Nz)
