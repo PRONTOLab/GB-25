d555b35737c03019f7dc8b1a09573fe8a4a4ff07
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-17 15:33:05 -0600

    Take first_time_step out of loop

--- ocean-climate-simulation/common.jl
+++ ocean-climate-simulation/common.jl
@@ -1,11 +1,15 @@
 using Reactant
 using Oceananigans
+ 
+const Δt = 30 # seconds
+    
+first_time_step!(model) = Oceananigans.TimeSteppers.first_time_step!(model, Δt)
 
 function loop!(model, Ninner)
-    Δt = 1200 # 20 minutes
-    Oceananigans.TimeSteppers.first_time_step!(model, Δt)
-    @trace for _ = 2:Ninner
+    Δt = 30 # seconds
+    @trace for _ = 1:Ninner
         Oceananigans.TimeSteppers.time_step!(model, Δt)
     end
     return nothing
 end
+
