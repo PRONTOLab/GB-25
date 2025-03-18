59145b62b8e73b6d94f22c23b58f72cad6d2db31
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-18 13:49:03 -0600

    Add a script with a simple case for sharding/meshing

---
+++ sharding/simple_sharding_problem.jl
@@ -0,0 +1,35 @@
+using Oceananigans
+using Reactant
+
+arch = Oceananigans.Architectures.ReactantState()
+Nx = Ny = Nz = 128
+Nz = 16
+grid = TripolarGrid(arch; size=(Nx, Ny, Nz), halo=(7, 7, 7), z=(0, 1))
+
+function mtn₁(λ, φ)
+    λ₁ = 70
+    φ₁ = 55
+    dφ = 5
+    return exp(-((λ - λ₁)^2 + (φ - φ₁)^2) / 2dφ^2)
+end
+
+function mtn₂(λ, φ)
+    λ₁ = 70
+    λ₂ = λ₁ + 180
+    φ₂ = 55
+    dφ = 5
+    return exp(-((λ - λ₂)^2 + (φ - φ₂)^2) / 2dφ^2)
+end
+
+gaussian_islands(λ, φ) = 2 * (mtn₁(λ, φ) + mtn₂(λ, φ))
+grid = ImmersedBoundaryGrid(grid, GridFittedBottom(gaussian_islands))
+
+c = CenterField(grid)
+∇²c = Field(∂x(∂x(c)) + ∂y(∂y(c)))
+
+cᵢ(λ, φ, z) = exp(-(λ^2 + φ^2) / 200)
+set!(c, cᵢ)
+Oceananigans.BoundaryConditions.fill_halo_regions!(c)
+compute!(∇²c)
+parent(∇²c)
+
