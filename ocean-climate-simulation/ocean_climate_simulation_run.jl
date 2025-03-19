5d3cde2572aae8cb458be2c1b4acbfe4060087d5
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-03-19 18:39:47 +0000

    Compile function to be profiled with `sync=true` (#94)

--- ocean-climate-simulation/ocean_climate_simulation_run.jl
+++ ocean-climate-simulation/ocean_climate_simulation_run.jl
@@ -12,7 +12,7 @@ model = data_free_ocean_climate_model_init(ReactantState())
 GC.gc(true); GC.gc(false); GC.gc(true)
 
 @info "Compiling..."
-rloop! = @compile raise=true loop!(model, Ninner)
+rloop! = @compile raise=true sync=true loop!(model, Ninner)
 
 @info "Running..."
 Reactant.with_profiler("./") do
