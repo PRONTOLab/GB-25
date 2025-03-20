54b3daff30f991900788b8f10a7ebb4d0ae8cf14
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-03-20 02:57:26 +0000

    [CI] Always upload MLIR modules (#95)
    * [CI] Always upload MLIR modules
    
    * [CI] Temporarily increase frequency of scheduled job to hourly
    
    This is to increase the chances of catching the broken IR.
    
    [skip tests]

--- ocean-climate-simulation/common.jl
+++ ocean-climate-simulation/common.jl
@@ -11,3 +11,9 @@ function loop!(model, Ninner)
 end
 
 Ninner = ConcreteRNumber(2)
+
+# If we are in GitHub Actions, make `TMPDIR` be a local directory from which we
+# can upload artifacts at the end.
+if get(ENV, "GITHUB_ACTIONS", "false") == "true"
+    ENV["TMPDIR"] = mkpath(joinpath(@__DIR__, "tmp"))
+end
