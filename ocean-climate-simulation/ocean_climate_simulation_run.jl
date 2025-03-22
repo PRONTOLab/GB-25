23175e53d54af79cd3bd05fe64078edf44356aff
Mosè Giordano <mose@gnu.org>
2025-03-22 19:18:59 +0000

    Make directory persistent

--- ocean-climate-simulation/ocean_climate_simulation_run.jl
+++ ocean-climate-simulation/ocean_climate_simulation_run.jl
@@ -1,6 +1,6 @@
 include("common.jl")
 
-write(@show(joinpath(mktempdir(), "module.mlir")), """
+write(@show(joinpath(mktempdir(; cleanup=false), "module.mlir")), """
 #loc = loc(unknown)
 module @reactant_Base.Br... attributes {mhlo.num_partitions = 1 : i64, mhlo.num_replicas = 1 : i64} {
   func.func @main(%arg0: tensor<1xf32> {reactant.donated} loc(unknown)) -> tensor<1xf32> {
