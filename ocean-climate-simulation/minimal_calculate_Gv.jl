1a5c95764213b154fd8d61ff685b69b5d522db30
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-21 19:09:11 -0600

    Add an mwe for correctness bug with calculate Gv

---
+++ ocean-climate-simulation/minimal_calculate_Gv.jl
@@ -0,0 +1,114 @@
+using Oceananigans
+using Oceananigans.Architectures: ReactantState
+using Reactant
+using Oceananigans.Models.HydrostaticFreeSurfaceModels.SplitExplicitFreeSurfaces:
+    calculate_substeps,
+    calculate_adaptive_settings
+
+function simple_model(arch)
+
+    Nx = Ny = Nz = 16
+    grid = LatitudeLongitudeGrid(arch,
+                                 topology = (Periodic, Bounded, Bounded),
+                                 size = (Nx, Ny, Nz),
+                                 longitude = (-10, 10),
+                                 latitude = (40, 60),
+                                 z = (-1000, 0),
+                                 halo = (6, 6, 6))
+
+    #momentum_advection = WENOVectorInvariant(order=5)
+    #momentum_advection = WENO(order=5)
+    momentum_advection = VectorInvariant()
+    free_surface = ExplicitFreeSurface(gravitational_acceleration=1)
+    model = HydrostaticFreeSurfaceModel(; grid, free_surface, momentum_advection)
+    model.clock.last_Δt = 60
+
+    return model
+end
+
+c_model = simple_model(CPU())
+r_model = simple_model(Oceananigans.Architectures.ReactantState())
+
+using Random
+Random.seed!(123)
+puc = parent(c_model.velocities.u)
+pur = parent(r_model.velocities.u)
+pvc = parent(c_model.velocities.v)
+pvr = parent(r_model.velocities.v)
+pwc = parent(c_model.velocities.w)
+pwr = parent(r_model.velocities.w)
+u₀ = rand(size(puc)...)
+v₀ = rand(size(pvc)...)
+w₀ = rand(size(pwc)...)
+
+# NB: The correctness issue requires setting v, w and seems to be triggered
+# by either WENOVectorInvariant or WENO. This suggests that the code
+# generated within `advective_momentum_flux_Wv` is problematic.
+#copyto!(puc, u₀)
+copyto!(pvc, v₀)
+copyto!(pwc, w₀)
+#copyto!(pur, Reactant.to_rarray(u₀))
+copyto!(pvr, Reactant.to_rarray(v₀))
+copyto!(pwr, Reactant.to_rarray(w₀))
+
+using Oceananigans.Models.HydrostaticFreeSurfaceModels:
+    interior_tendency_kernel_parameters,
+    immersed_boundary_condition,
+    get_active_cells_map,
+    compute_hydrostatic_free_surface_Gu!,
+    compute_hydrostatic_free_surface_Gv!
+
+using Oceananigans.Utils: launch!
+
+function launch_problem_kernel!(model)
+    active_cells_map = get_active_cells_map(model.grid, Val(:interior))
+    velocities = model.velocities
+    grid = model.grid
+    arch = Oceananigans.Architectures.architecture(grid)
+    kernel_parameters = interior_tendency_kernel_parameters(arch, grid)
+
+    u_immersed_bc = immersed_boundary_condition(velocities.u)
+    v_immersed_bc = immersed_boundary_condition(velocities.v)
+
+    u_forcing = model.forcing.u
+    v_forcing = model.forcing.v
+
+    start_momentum_kernel_args = (model.advection.momentum,
+                                  model.coriolis,
+                                  model.closure)
+
+    end_momentum_kernel_args = (velocities,
+                                model.free_surface,
+                                model.tracers,
+                                model.buoyancy,
+                                model.diffusivity_fields,
+                                model.pressure.pHY′,
+                                model.auxiliary_fields,
+                                model.vertical_coordinate,
+                                model.clock)
+
+    u_kernel_args = tuple(start_momentum_kernel_args..., u_immersed_bc, end_momentum_kernel_args..., u_forcing)
+    v_kernel_args = tuple(start_momentum_kernel_args..., v_immersed_bc, end_momentum_kernel_args..., v_forcing)
+
+    launch!(arch, grid, kernel_parameters,
+            compute_hydrostatic_free_surface_Gv!, model.timestepper.Gⁿ.v, grid, 
+            active_cells_map, v_kernel_args;
+            active_cells_map)
+
+    return nothing
+end
+
+r_problem! = @compile sync=true raise=true launch_problem_kernel!(r_model)
+@time r_problem!(r_model)
+@time launch_problem_kernel!(c_model)
+
+function compare(c, r, name="")
+    pc = Array(parent(c))
+    pr = Array(parent(r))
+    @show name, maximum(pc .- pr)
+end
+
+Gvc = c_model.timestepper.Gⁿ.v
+Gvr = r_model.timestepper.Gⁿ.v
+compare(Gvc, Gvr, "Gv")
+
