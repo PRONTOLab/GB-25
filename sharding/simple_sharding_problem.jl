a00d75e2b4173426cbb27fe8ebcbf916d4dd755f
Avik Pal <avikpal@mit.edu>
2025-03-22 22:17:40 -0500

    feat: sharding example working :tada:

--- sharding/simple_sharding_problem.jl
+++ sharding/simple_sharding_problem.jl
@@ -1,30 +1,42 @@
 # /home/avik-pal/.julia/bin/mpiexecjl -np 4 --project=. julia --threads=32 --color=yes --startup=no GB-25/sharding/simple_sharding_problem.jl
 
+# mkpath("xla_dumps")
+# tmpname = tempname("xla_dumps")
+
 ENV["CUDA_VISIBLE_DEVICES"] = ""
-ENV["XLA_FLAGS"] = "--xla_force_host_platform_device_count=4"
+# ENV["XLA_FLAGS"] = "--xla_force_host_platform_device_count=4"
+# ENV["XLA_FLAGS"] = " --xla_dump_to=xla_dumps/$(tmpname)/"
 ENV["JULIA_DEBUG"] = "Reactant_jll,Reactant"
 
-# using MPI
-# MPI.Init()  # Only needed if using MPI to detect the coordinator
+# @info ENV["XLA_FLAGS"]
+
+using MPI
+MPI.Init()  # Only needed if using MPI to detect the coordinator
 
 using Oceananigans
 using Reactant
 
-# Reactant.Distributed.initialize()
+# using Cthulhu
+
+Reactant.Distributed.initialize()
 
 ndevices = length(Reactant.devices())
 nxdevices = floor(Int, sqrt(ndevices))
 nydevices = ndevices ÷ nxdevices
+
+process_id = Reactant.Distributed.local_rank()
 
 arch = Oceananigans.Distributed(
     Oceananigans.ReactantState();
     partition=Partition(nxdevices, nydevices, 1)
 )
 
+# arch = Oceananigans.ReactantState()
+
 Nx = Ny = 16
 Nz = 4
 
-@info "creating tripolar grid"
+@info "[$(process_id)] creating tripolar grid"
 grid = TripolarGrid(arch; size=(Nx, Ny, Nz), halo=(7, 7, 7), z=(0, 1))
 
 function mtn₁(λ, φ)
@@ -44,84 +56,20 @@ end
 
 gaussian_islands(λ, φ) = 2 * (mtn₁(λ, φ) + mtn₂(λ, φ))
 
-@info "creating immersed boundary grid"
-grid = ImmersedBoundaryGrid(grid, GridFittedBottom(gaussian_islands)) # xxx?
+@info "[$(process_id)] creating immersed boundary grid"
+grid = ImmersedBoundaryGrid(grid, GridFittedBottom(gaussian_islands))
 
-model = HydrostaticFreeSurfaceModel(; grid)
-model.clock.last_Δt = 60
+free_surface = SplitExplicitFreeSurface(substeps=3)
+model = HydrostaticFreeSurfaceModel(; grid, free_surface)
+# model = HydrostaticFreeSurfaceModel(; grid)
 
-@info "compiling first time step"
-compiled_first_time_step! = @compile optimize=true raise=true Oceananigans.TimeSteppers.first_time_step!(model)
+model.clock.last_Δt = ConcreteRNumber(60.0)
 
-@info "running first time step"
-compiled_first_time_step!(model)
+@info "[$(process_id)] compiling first time step"
+compiled_first_time_step! = @compile Oceananigans.TimeSteppers.first_time_step!(model, model.clock.last_Δt)
 
-exit(0)
+@info "[$(process_id)] running first time step"
+@time compiled_first_time_step!(model, model.clock.last_Δt)
+@time compiled_first_time_step!(model, model.clock.last_Δt)
 
-# c = CenterField(grid);
-# ∇²c = Field(∂x(∂x(c)) + ∂y(∂y(c)));
-
-@info "setting"
-cᵢ(λ, φ, z) = exp(-(λ^2 + φ^2) / 200)
-set!(c, cᵢ)
-
-@info "filling halo regions"
-@jit Oceananigans.BoundaryConditions.fill_halo_regions!(c)
-
-# res = @jit compute!(∇²c);
-
-@info "compiling"
-# if Reactant.Distributed.local_rank() == 0
-hlo = @code_hlo compute!(∇²c);
-# end
-
-if Reactant.Distributed.local_rank() == 0
-    println(hlo)
-end
-
-# parent(∇²c)
-
-
-# #### Run on CPU
-
-# cpu_arch = CPU()
-# grid_cpu = TripolarGrid(cpu_arch; size=(Nx, Ny, Nz), halo=(7, 7, 7), z=(0, 1))
-
-# c_cpu = CenterField(grid_cpu);
-# ∇²c_cpu = Field(∂x(∂x(c_cpu)) + ∂y(∂y(c_cpu)));
-
-# res_cpu = compute!(∇²c_cpu);
-
-
-# #### Run with ReactantState
-# using Reactant, Oceananigans
-
-# Nx = Ny = Nz = 128
-
-# r_arch = Oceananigans.ReactantState()
-# grid_r = TripolarGrid(r_arch; size=(Nx, Ny, Nz), halo=(7, 7, 7), z=(0, 1))
-
-# c_r = CenterField(grid_r);
-# ∇²c_r = Field(∂x(∂x(c_r)) + ∂y(∂y(c_r)));
-
-# res_r = @jit compute!(∇²c_r);
-
-
-# # Resharding Problem
-# using Reactant
-
-# struct MyStruct{A}
-#     a::A
-# end
-
-# mesh = Sharding.Mesh(reshape(Reactant.devices(), 2, :), (:x, :y))
-
-# x = Reactant.to_rarray(MyStruct(rand(2, 2)))
-# y = Reactant.to_rarray(
-#     rand(2, 2); sharding=Sharding.NamedSharding(mesh, (:x, :y))
-# )
-
-# function fn(a::MyStruct, b)
-#     a.a .= a.a .+ b
-#     return a
-# end
+# exit(0)
