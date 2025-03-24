4acbcbced98da9b5a6ba7fa176fa7043e5e306f9
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-24 09:05:32 -0600

    Update submit script

--- sharding/submit.sh
+++ sharding/submit.sh
@@ -1,25 +1,23 @@
 #!/bin/bash -l
-#
-#SBATCH --account=g191
-#SBATCH --job-name=sharding-reactant
-#SBATCH --time=00:30:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=4
+
+#SBATCH --job-name="scaling_test_8"
+#SBATCH --output=slurm.%j.o
+#SBATCH --error=slurm.%j.e
+#SBATCH --time=00:40:00
+#SBATCH --nodes=2
+#SBATCH --ntasks=2
 #SBATCH --gpus-per-node=4
-#SBATCH --cpus-per-task=1
-#SBATCH --output sharding-reactant-out.txt
-#SBATCH --error sharding-reactant-err.txt
+#SBATCH --ntasks-per-node=1
 #SBATCH --constraint=gpu
 #SBATCH --exclusive
-
-##SBATCH --partition=debug
-
+        
+#SBATCH --account=g191
+export Ngpu=8
+export resolution_fraction=32
 export JULIA_DEBUG="Reactant,Reactant_jll"
-alias julia='/capstor/scratch/cscs/gwagner/daint/juliaup/bin/julia'
+export MPICH_GPU_SUPPORT_ENABLED=1
+
 ulimit -s unlimited
-
-# Important else XLA might hang indefinitely
-unset no_proxy http_proxy https_proxy NO_PROXY HTTP_PROXY HTTPS_PROXY
-
+alias julia='/capstor/scratch/cscs/gwagner/daint/juliaup/bin/julia'
 srun --preserve-env --gpu-bind=per_task:1 --cpu_bind=sockets bash unset_then_launch.sh
-
+                
