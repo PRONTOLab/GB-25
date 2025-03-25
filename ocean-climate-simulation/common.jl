104643fc96c0c0361117b404fdc1e0f75c4a2f36
William Moses <me@wsmoses.com>
2025-03-24 23:29:30 -0500

    try track_numbers=false

--- ocean-climate-simulation/common.jl
+++ ocean-climate-simulation/common.jl
@@ -9,7 +9,7 @@ end
 
 function loop!(model, Ninner)
     Δt = model.clock.last_Δt
-    @trace for _ = 1:Ninner
+    @trace track_numbers=false for _ = 1:Ninner
         Oceananigans.TimeSteppers.time_step!(model, Δt)
     end
     return nothing
