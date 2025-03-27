82ebed905b2b292a22fcfcdcc10fc26b8ccbc7fc
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-27 11:35:10 -0600

    Add a new baroclinic instability model

---
+++ src/model_utils.jl
@@ -0,0 +1,105 @@
+using Reactant
+
+using Oceananigans
+using Oceananigans.Units
+using Oceananigans.Architectures: Architectures
+using SeawaterPolynomials
+
+using ClimaOcean
+using ClimaOcean.OceanSeaIceModels.InterfaceComputations: FixedIterations, ComponentInterfaces
+
+using CFTime
+using Dates
+using Printf
+using Profile
+using Random
+using Serialization
+
+OceananigansReactantExt = Base.get_extension(Oceananigans, :OceananigansReactantExt)
+
+const PROFILE = Ref(false)
+
+macro gbprofile(name::String, expr::Expr)
+    return quote
+        if $(PROFILE)[]
+            $(Profile.clear)()
+            $(Profile.init)(; delay=0.1)
+            out = $(Profile).@profile $(esc(expr))
+            open(string("profile_", $(esc(name)), ".txt"), "w") do s
+                println(s, "# Showing profile of")
+                println(s, "#     ", $(string(expr)))
+                println(s, "# at ", $(string(__source__)))
+                $(Profile.print)(IOContext(s, :displaysize => (48, 1000)))
+            end
+            $(Serialization.serialize)(string("profile_", $(esc(name)), ".dat"), $(Profile).retrieve())
+            $(Profile.clear)()
+            out
+        else
+            $(esc(expr))
+        end
+    end
+end
+
+function simple_latitude_longitude_grid(arch, resolution, Nz)
+    Nx = convert(Int, 360 / resolution)
+    Ny = convert(Int, 160 / resolution)
+
+    z_faces = exponential_z_faces(; Nz, depth=4000, h=30) # may need changing for very large Nz
+
+    grid = LatitudeLongitudeGrid(arch, size=(Nx, Ny, Nz), halo=(7, 7, 7), z=z_faces,
+        latitude = (-80, 80),
+        longitude = (0, 360)
+    )
+
+    return grid
+end
+
+function mtn₁(λ, φ)
+    λ₁ = 70
+    φ₁ = 55
+    dφ = 5
+    return exp(-((λ - λ₁)^2 + (φ - φ₁)^2) / 2dφ^2)
+end
+
+function mtn₂(λ, φ)
+    λ₁ = 70
+    λ₂ = λ₁ + 180
+    φ₂ = 55
+    dφ = 5
+    return exp(-((λ - λ₂)^2 + (φ - φ₂)^2) / 2dφ^2)
+end
+
+# Simple initial condition for producing pretty pictures
+function smooth_step(φ)
+    φ₀ = 40
+    dφ = 5
+    return (1 - tanh((abs(φ) - φ₀) / dφ)) / 2
+end
+
+function Tᵢ(λ, φ, z)
+    dTdz = 1e-3
+    return (30 + dTdz * z) * smooth_step(φ) + rand()
+end
+
+function Sᵢ(λ, φ, z)
+    dSdz = - 5e-3
+    return dSdz * z + rand()
+end
+
+function gaussian_islands_tripolar_grid(arch::Architectures.AbstractArchitecture, resolution, Nz)
+    Nx = convert(Int, 360 / resolution)
+    Ny = convert(Int, 180 / resolution)
+
+    # Grid setup
+    z_faces = exponential_z_faces(; Nz, depth=4000, h=30) # may need changing for very large Nz
+    underlying_grid = TripolarGrid(arch; size=(Nx, Ny, Nz), halo=(7, 7, 7), z=z_faces)
+
+    zb = z_faces[1]
+    h = -zb + 100
+    gaussian_islands(λ, φ) = zb + h * (mtn₁(λ, φ) + mtn₂(λ, φ))
+
+    return @gbprofile "ImmersedBoundaryGrid" ImmersedBoundaryGrid(underlying_grid,
+                                                                  GridFittedBottom(gaussian_islands);
+                                                                  active_cells_map = false)
+end
+
