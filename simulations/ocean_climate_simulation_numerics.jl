53c37b6bcfd6bc5e8d60a4ef5e5cfa06bf4ef74f
Nora Loose <nora.loose@gmail.com>
2025-03-27 12:48:44 -0700

    Include passive tracers and tracer diagnostics

--- simulations/ocean_climate_simulation_numerics.jl
+++ simulations/ocean_climate_simulation_numerics.jl
@@ -3,47 +3,50 @@ using Oceananigans
 using Oceananigans.Units
 using Printf
 
-# Tunable parameters
-FT = Float64
-
-resolution = 2 # degree
-Nz = 20 # vertical levels
-
-momentum_advection_order = 5
-tracer_advection_order = 5
-vorticity_order = nothing
-
-target_dir = "/pscratch/sd/n/nloose/GB-25/simulations/" # change to where you want save stuff
-
 # Generate prefix using parameter values
 if resolution isa Rational
     res_str = "$(numerator(resolution))_$(denominator(resolution))"
 else
     res_str = "$(Int(resolution))"
 end
+
+if arch == GPU()
+    arch_str = "GPU"
+elseif arch == CPU()
+    arch_str = "CPU"
+elseif arch == ReactantState()
+    arch_str = "Reactant"
+end
+    
 vort_str = vorticity_order !== nothing ? "_vort$(vorticity_order)" : ""
 
 prefix = (
     "Degree$(res_str)_Nz$(Nz)_"
     * "momadv$(momentum_advection_order)_tradv$(tracer_advection_order)"
-    * vort_str * "_$(FT)"
+    * vort_str * "_$(FT)_$(arch_str)"
     )
 
-arch = GPU()
+# Some more parameters that we will NOT change for the numerics tests
 zstar_vertical_coordinate = true
+include_passive_tracers = true
 
+# Set precision
 Oceananigans.defaults.FloatType = FT
 
 # Spinup with small time step
-Δt = 120 # seconds
+model = ocean_climate_model_init(arch; 
+    resolution=resolution, 
+    Nz=Nz, 
+    Δt=Δt₁,
+    tracer_advection_order=tracer_advection_order,
+    vorticity_order=vorticity_order,
+    momentum_advection_order=momentum_advection_order,
+    zstar_vertical_coordinate=zstar_vertical_coordinate,
+    include_passive_tracers=include_passive_tracers
+)
 
-model = ocean_climate_model_init(arch; resolution, Nz, Δt,
-                                 tracer_advection_order,
-                                 vorticity_order,
-                                 momentum_advection_order,
-                                 zstar_vertical_coordinate)
-
-simulation = Simulation(model; Δt, stop_time=10days)
+# Run spin-up of 30 days with small time step
+simulation = Simulation(model; Δt=Δt₁, stop_time=30days)
 
 # Callbacks
 wall_clock = Ref(time_ns())
@@ -65,24 +68,66 @@ function progress(sim)
     return nothing
 end
 
-add_callback!(simulation, progress, TimeInterval(1days))
+add_callback!(simulation, progress, TimeInterval(10days))
 
-# Diagnostics
+# Integral diagnostics
 u, v, w = model.ocean.model.velocities
 e = @at (Center, Center, Center) (u^2 + v^2) / 2
-E = Integral(e, dims=(1, 2, 3))
+kinetic_energy_integral = Integral(e, dims=(1, 2, 3))
 
-ke_ow = JLD2Writer(
+c_surface, c_bottom = model.ocean.model.tracers.C_surface, model.ocean.model.tracers.C_bottom 
+
+surface_tracer_integral = Integral(c_surface, dims=(1, 2, 3))
+bottom_tracer_integral = Integral(c_bottom, dims=(1, 2, 3))
+surface_tracer_variance_integral = Integral(c_surface^2, dims=(1, 2, 3))
+bottom_tracer_variance_integral = Integral(c_bottom^2, dims=(1, 2, 3))
+
+integral_ow = JLD2Writer(
     model.ocean.model,
-    (; E),
-    filename = "$(prefix)_kinetic_energy.jld2",
+    (; kinetic_energy_integral, surface_tracer_integral, bottom_tracer_integral, surface_tracer_variance_integral, bottom_tracer_variance_integral),
+    filename = "$(prefix)_integrals",
     dir = target_dir,
     schedule = AveragedTimeInterval(1days; window=1days, stride=1),
     overwrite_existing = true,
     array_type = Array{FT}
 )
-simulation.output_writers[:ke] = ke_ow
+simulation.output_writers[:integral] = integral_ow
 
+# Surface field diagnostics
+outputs = merge(model.ocean.model.tracers, model.ocean.model.velocities)
+
+surface_field_ow = JLD2Writer(
+    model.ocean.model, 
+    outputs,
+    filename ="$(prefix)_surface_fields",
+    dir = target_dir,
+    indices = (:, :, Nz),
+    schedule = AveragedTimeInterval(1days; window=1days, stride=1),
+    overwrite_existing = true,
+    array_type = Array{FT}
+)
+simulation.output_writers[:surface] = surface_field_ow
+
+# Vertically integrated tracer diagnostics
+surface_tracer_vertical_integral = Integral(c_surface, dims=(3))
+bottom_tracer_vertical_integral = Integral(c_bottom, dims=(3))
+
+vertical_integral_ow = JLD2Writer(
+    model.ocean.model, 
+    (; surface_tracer_vertical_integral, bottom_tracer_vertical_integral),
+    filename ="$(prefix)_vertically_integrated_tracers",
+    dir = target_dir,
+    indices = (:, :, Nz),
+    schedule = AveragedTimeInterval(1days; window=1days, stride=1),
+    overwrite_existing = true,
+    array_type = Array{FT}
+)
+simulation.output_writers[:vertical_integral] = vertical_integral_ow
+
+# Run the simulation
 run!(simulation)
 
-# The real simulation with a larger time step
+# Proceed simulation with a larger time step
+simulation.stop_time = 730days
+simulation.Δt = Δt₂
+run!(simulation)
