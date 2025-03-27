1fae04af835eca654e28a5349757c6c6c1b40da3
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-26 23:23:01 -0600

    Pretty up

--- baroclinic-instability-simulation/baroclinic_instability_run.jl
+++ baroclinic-instability-simulation/baroclinic_instability_run.jl
@@ -43,11 +43,6 @@ GordonBell25.sync_states!(r_model, c_model)
 @info "Comparing regular and Reactant model on the grid"
 @show c_model.grid
 
-@time "Regular initialize" Oceananigans.initialize!(c_model)
-@time "Regular update state" Oceananigans.TimeSteppers.update_state!(c_model)
-@time "Regular first time step" first_time_step!(c_model)
-@time "Regular ten step loop" loop!(c_model, 10)
-
 @info "The Reactant model is"
 @show r_model
 
@@ -56,18 +51,16 @@ GordonBell25.compare_states(r_model, c_model)
 
 GC.gc(true); GC.gc(false); GC.gc(true)
 
-@info "Writing unoptimized mlir"
-unopt_update_state = @code_hlo optimize=false Oceananigans.TimeSteppers.update_state!(r_model)
-open("unopt_update_state.mlir", "w") do io
-    show(IOContext(io, :debug => true), unopt_update_state)
-end
-
 @info "Compiling update_state..."
 r_initialize! = @compile sync=true raise=raise Oceananigans.initialize!(r_model)
 r_update_state! = @compile sync=true raise=raise Oceananigans.TimeSteppers.update_state!(r_model)
 
 @time "Reactant initialize" r_initialize!(r_model)
+@time "Regular initialize" Oceananigans.initialize!(c_model)
+
 @time "Reactant update state" r_update_state!(r_model)
+@time "Regular update state" Oceananigans.TimeSteppers.update_state!(c_model)
+
 @time "Regular initialize" Oceananigans.initialize!(c_model)
 @time "Regular update state" Oceananigans.TimeSteppers.update_state!(c_model)
 
@@ -81,6 +74,7 @@ r_first_time_step! = @compile sync=true raise=raise first_time_step!(r_model)
 r_loop! = @compile sync=true raise=raise loop!(r_model, ConcreteRNumber(10))
 
 @time "First Reactant time step" r_first_time_step!(r_model)
+@time "Regular first time step" first_time_step!(c_model)
 
 @info "After first time step:"
 GordonBell25.compare_states(r_model, c_model)
