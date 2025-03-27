deb94a65f9dc8732645149605b6c1f6e9b80625e
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-26 23:26:01 -0600

    Update run script for only Reactant

--- baroclinic-instability-simulation/baroclinic_instability_run.jl
+++ baroclinic-instability-simulation/baroclinic_instability_run.jl
@@ -18,7 +18,7 @@ Oceananigans.defaults.FloatType = Float32
 raise = true
 
 configuration = (;
-    Δt                 = 1.0, #10minutes,
+    Δt                 = 1minutes, #10minutes,
     resolution         = 2,
     Nz                 = 50,
     # closure            = Oceananigans.TurbulenceClosures.CATKEVerticalDiffusivity(),
@@ -33,21 +33,12 @@ configuration = (;
 
 @show configuration
 
-r_arch = ReactantState()
-c_arch = GPU(Metal.MetalBackend())
-r_model = GordonBell25.baroclinic_instability_model(r_arch; configuration...)
-c_model = GordonBell25.baroclinic_instability_model(c_arch; configuration...)
-GordonBell25.sync_states!(r_model, c_model)
+arch = ReactantState()
+r_model = GordonBell25.baroclinic_instability_model(arch; configuration...)
 @show r_model isa OceananigansReactantExt.Models.ReactantHFSM 
-
-@info "Comparing regular and Reactant model on the grid"
-@show c_model.grid
 
 @info "The Reactant model is"
 @show r_model
-
-@info "Comparing states before compilation or time stepping"
-GordonBell25.compare_states(r_model, c_model)
 
 GC.gc(true); GC.gc(false); GC.gc(true)
 
@@ -55,36 +46,14 @@ GC.gc(true); GC.gc(false); GC.gc(true)
 r_initialize! = @compile sync=true raise=raise Oceananigans.initialize!(r_model)
 r_update_state! = @compile sync=true raise=raise Oceananigans.TimeSteppers.update_state!(r_model)
 
-@time "Reactant initialize" r_initialize!(r_model)
-@time "Regular initialize" Oceananigans.initialize!(c_model)
-
-@time "Reactant update state" r_update_state!(r_model)
-@time "Regular update state" Oceananigans.TimeSteppers.update_state!(c_model)
-
-@time "Regular initialize" Oceananigans.initialize!(c_model)
-@time "Regular update state" Oceananigans.TimeSteppers.update_state!(c_model)
-
-@info "After initialize and update state:"
-GordonBell25.compare_states(r_model, c_model)
-
 @info "Compiling first time step..."
 r_first_time_step! = @compile sync=true raise=raise first_time_step!(r_model)
 
-@info "Compiling time step..."
+@info "Compiling loop..."
 r_loop! = @compile sync=true raise=raise loop!(r_model, ConcreteRNumber(10))
 
+@time "Reactant initialize" r_initialize!(r_model)
+@time "Reactant update state" r_update_state!(r_model)
 @time "First Reactant time step" r_first_time_step!(r_model)
-@time "Regular first time step" first_time_step!(c_model)
-
-@info "After first time step:"
-GordonBell25.compare_states(r_model, c_model)
-
-@info "Synchronizing states to test effect of initialization:"
-GordonBell25.sync_states!(r_model, c_model)
-
 @time "Reactant ten step loop" r_loop!(r_model, ConcreteRNumber(10))
-@time "Regular ten step loop" loop!(c_model, 10)
-
-@info "After ten time steps:"
-GordonBell25.compare_states(r_model, c_model)
 
