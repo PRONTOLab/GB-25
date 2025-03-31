5c44e486b5b7c75460fe5637dad8e03c2c147d54
Mosè Giordano <mose@gnu.org>
2025-04-01 00:34:14 +0100

    Expand simple sharding script
    [skip tests]

--- sharding/simple_sharding_problem.jl
+++ sharding/simple_sharding_problem.jl
@@ -1,23 +1,19 @@
-# /home/avik-pal/.julia/bin/mpiexecjl -np 4 --project=. julia --threads=32 --color=yes --startup=no GB-25/sharding/simple_sharding_problem.jl
+using Dates
+@info "This is where the fun begins" now(UTC)
 
-# mkpath("xla_dumps")
-# tmpname = tempname("xla_dumps")
-
-# ENV["CUDA_VISIBLE_DEVICES"] = ""
-# ENV["XLA_FLAGS"] = "--xla_force_host_platform_device_count=4"
-# ENV["XLA_FLAGS"] = " --xla_dump_to=xla_dumps/$(tmpname)/"
 ENV["JULIA_DEBUG"] = "Reactant_jll,Reactant"
-
-# @info ENV["XLA_FLAGS"]
-
-# using MPI
-# MPI.Init()  # Only needed if using MPI to detect the coordinator
 
 using Oceananigans
 using Reactant
 
-# using Cthulhu
-using Dates
+using Libdl: dllist
+
+@show filter(contains("nccl"), dllist())
+
+Reactant.MLIR.IR.DUMP_MLIR_ALWAYS[] = true
+Reactant.MLIR.IR.DUMP_MLIR_DIR[] = joinpath(@__DIR__, "mlir_dumps", string(ENV["SLURM_JOB_ID"], ".", ENV["SLURM_PROCID"]))
+Reactant.Compiler.DEBUG_DISABLE_RESHARDING[] = true
+Reactant.Compiler.DEBUG_PRINT_CODEGEN[] = true
 
 Reactant.Distributed.initialize()
 
@@ -34,9 +30,15 @@ arch = Oceananigans.Distributed(
 
 # arch = Oceananigans.ReactantState()
 
-Nx = Ny = 16
-Nz = 4
+@assert ispow2(ndevices)
+Nx, Ny = let
+    x = log2(ndevices) / 2
+    512 .* Int.(exp2.((floor(Int, x), ceil(Int, x))))
+end
+Nz = 128
 
+#=
+##### Tripolar Grid
 @info "[$(process_id)] creating tripolar grid" now(UTC)
 grid = TripolarGrid(arch; size=(Nx, Ny, Nz), halo=(7, 7, 7), z=(0, 1))
 
@@ -59,18 +61,102 @@ gaussian_islands(λ, φ) = 2 * (mtn₁(λ, φ) + mtn₂(λ, φ))
 
 @info "[$(process_id)] creating immersed boundary grid" now(UTC)
 grid = ImmersedBoundaryGrid(grid, GridFittedBottom(gaussian_islands))
+=#
 
-free_surface = SplitExplicitFreeSurface(substeps=3)
+@info "[$(process_id)] allocations" Reactant.XLA.allocatorstats()
+
+##### Latlong grid
+@info "[$(process_id)] creating latlong grid" now(UTC)
+grid = LatitudeLongitudeGrid(arch, size=(Nx, Ny, Nz), halo=(7, 7, 7), z=(-4000, 0),
+                             latitude = (-80, 80),
+                             longitude = (0, 360)
+                             )
+
+@info "[$(process_id)] allocations" Reactant.XLA.allocatorstats()
+
+free_surface = ExplicitFreeSurface()
 model = HydrostaticFreeSurfaceModel(; grid, free_surface)
 # model = HydrostaticFreeSurfaceModel(; grid)
 
+@show model
+
 model.clock.last_Δt = ConcreteRNumber(60.0)
 
-@info "[$(process_id)] compiling first time step" now(UTC)
-compiled_first_time_step! = @compile Oceananigans.TimeSteppers.first_time_step!(model, model.clock.last_Δt)
-compiled_time_step! = @compile Oceananigans.TimeSteppers.time_step!(model, model.clock.last_Δt)
+function first_time_step!(model)
+    Δt = model.clock.last_Δt
+    Oceananigans.TimeSteppers.first_time_step!(model, Δt)
+    return nothing
+end
 
-@info "[$(process_id)] running first time step" now(UTC)
-@time "[$(process_id)] first time step" compiled_first_time_step!(model, model.clock.last_Δt)
-@info "[$(process_id)] running second time step" now(UTC)
-@time "[$(process_id)] second time step" compiled_time_step!(model, model.clock.last_Δt)
+function loop!(model, Ninner)
+    Δt = model.clock.last_Δt
+    @trace track_numbers=false for _ = 1:Ninner
+        Oceananigans.TimeSteppers.time_step!(model, Δt)
+    end
+    return nothing
+end
+
+@info "[$(process_id)] allocations" Reactant.XLA.allocatorstats()
+
+# @info "[$(process_id)] compiling first time step" now(UTC)
+# compiled_first_time_step! = @compile Oceananigans.TimeSteppers.first_time_step!(model, model.clock.last_Δt)
+# @info "[$(process_id)] compiling second time step" now(UTC)
+# compiled_time_step! = @compile Oceananigans.TimeSteppers.time_step!(model, model.clock.last_Δt)
+# compiled_update_state! = @compile Oceananigans.TimeSteppers.update_state!(model)
+# @info "[$(process_id)] allocations" Reactant.XLA.allocatorstats()
+
+@info "[$(process_id)] compiling first time step" now(UTC)
+compiled_first_time_step! = @compile raise=true first_time_step!(model)
+@info "[$(process_id)] compiling loop" now(UTC)
+Ninner = ConcreteRNumber(2; sharding=Sharding.NamedSharding(arch.connectivity, ()))
+compiled_loop! = @compile raise=true loop!(model, Ninner)
+@info "[$(process_id)] allocations" Reactant.XLA.allocatorstats()
+
+# #-------------------------------------------------------------------------------
+# code = @code_hlo optimize=:before_raise first_time_step!(model)
+# open(joinpath(@__DIR__, "first_time_step_before_raise_$(process_id).mlir"), "w") do io
+#     show(IOContext(io, :debug => true), code)
+# end
+# code = @code_hlo optimize=true first_time_step!(model)
+# open(joinpath(@__DIR__, "first_time_step_optimised_$(process_id).mlir"), "w") do io
+#     show(IOContext(io, :debug => true), code)
+# end
+# code = @code_hlo optimize=:before_raise loop!(model, Ninner)
+# open(joinpath(@__DIR__, "loop_before_raise_$(process_id).mlir"), "w") do io
+#     show(IOContext(io, :debug => true), code)
+# end
+# code = @code_hlo optimize=true loop!(model, Ninner)
+# open(joinpath(@__DIR__, "loop_optimised_$(process_id).mlir"), "w") do io
+#     show(IOContext(io, :debug => true), code)
+# end
+# #-------------------------------------------------------------------------------
+
+profile_dir = joinpath(@__DIR__, "profiling", string(ENV["SLURM_JOB_ID"], ".", ENV["SLURM_PROCID"]))
+mkpath(profile_dir)
+Reactant.with_profiler(profile_dir) do
+
+    # @info "[$(process_id)] running first time step" now(UTC)
+    # @info "[$(process_id)] allocations" Reactant.XLA.allocatorstats()
+    # @time "[$(process_id)] first time step" compiled_first_time_step!(model, model.clock.last_Δt)
+    # @info "[$(process_id)] allocations" Reactant.XLA.allocatorstats()
+    # @info "[$(process_id)] running loop" now(UTC)
+    # @time "[$(process_id)] loop" for iter in 2:10
+    #     @info "[$(process_id)] iterating" iter now(UTC)
+    #     @time "[$(process_id)] $(iter)-th timestep" compiled_update_state!(model)
+    #     if iter < 10 || iszero(iter % 50)
+    #         @info "[$(process_id)] allocations" iter Reactant.XLA.allocatorstats()
+    #     end
+    # end
+
+    Ninner = ConcreteRNumber(10; sharding=Sharding.NamedSharding(arch.connectivity, ()))
+    @info "[$(process_id)] running first time step" now(UTC)
+    @info "[$(process_id)] allocations" Reactant.XLA.allocatorstats()
+    @time "[$(process_id)] first time step" compiled_first_time_step!(model)
+    @info "[$(process_id)] allocations" Reactant.XLA.allocatorstats()
+    @info "[$(process_id)] running loop" now(UTC)
+    @time "[$(process_id)] loop" compiled_loop!(model, Ninner)
+    @info "[$(process_id)] allocations" Reactant.XLA.allocatorstats()
+
+end
+
+@info "Done!" now(UTC)
