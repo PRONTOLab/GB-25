ee16293213dc07f463db8a915e9da0f9d0182953
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-31 22:30:37 -0600

    Add tracer only model

---
+++ simulations/tracer_only_simulation_run.jl
@@ -0,0 +1,46 @@
+using GordonBell25
+using GordonBell25: first_time_step!, time_step!, loop!, preamble
+using Oceananigans
+using Oceananigans.Architectures: ReactantState
+using Reactant
+
+# Reactant.Compiler.SROA_ATTRIBUTOR[] = false
+# Reactant.MLIR.IR.DUMP_MLIR_ALWAYS[] = true
+
+preamble()
+
+Nt = ConcreteRNumber(3)
+Oceananigans.defaults.FloatType = Float32
+
+@info "Generating model..."
+arch = ReactantState()
+model = GordonBell25.tracer_only_model(arch, Nx=32, Ny=32, Nz=32, Δt=1e-2)
+
+GC.gc(true); GC.gc(false); GC.gc(true)
+
+#=
+@time first_time_step!(model)
+@time time_step!(model)
+@time time_step!(model)
+@time time_step!(model)
+@time loop!(model, 10)
+@time loop!(model, 10)
+@time loop!(model, 10)
+=#
+
+@info "Compiling..."
+rfirst! = @compile raise=true sync=true first_time_step!(model)
+rstep! = @compile raise=true sync=true time_step!(model)
+rloop! = @compile raise=true sync=true loop!(model, Ninner)
+
+@info "Running..."
+Reactant.with_profiler("./") do
+    rfirst!(model)
+end
+Reactant.with_profiler("./") do
+    rstep!(model)
+end
+Reactant.with_profiler("./") do
+    rloop!(model, Ninner)
+end
+@info "Done!"
--- src/GordonBell25.jl
+++ src/GordonBell25.jl
@@ -6,7 +6,9 @@ include("model_utils.jl")
 include("timestepping_utils.jl")
 include("data_free_ocean_climate_model.jl")
 include("baroclinic_instability_model.jl")
+include("tracer_only_model.jl")
 include("sharding_utils.jl")
 include("precompile.jl")
+include("correctness.jl")
 
 end # module
