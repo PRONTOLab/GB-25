795ab9de9f5c7cc6a5baacaf2dfb3f3a8d16a6c9
William Moses <me@wsmoses.com>
2025-04-01 01:23:17 -0500

    Update baroclinic_instability_simulation_run.jl

--- simulations/baroclinic_instability_simulation_run.jl
+++ simulations/baroclinic_instability_simulation_run.jl
@@ -5,7 +5,8 @@ using Oceananigans.Architectures: ReactantState
 using Reactant
 
 # Reactant.Compiler.SROA_ATTRIBUTOR[] = false
-Reactant.MLIR.IR.DUMP_MLIR_ALWAYS[] = true
+Reactant.Compiler.DUMP_FAILED_LOCKSTEP[] = true
+# Reactant.MLIR.IR.DUMP_MLIR_ALWAYS[] = true
 
 preamble()
 
