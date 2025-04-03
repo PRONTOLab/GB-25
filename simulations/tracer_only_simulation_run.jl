94ff28746c67347065bf9f36151e8413c6ea9e03
Gregory Wagner <wagner.greg@gmail.com>
2025-04-03 08:03:37 -0400

    Make easier to run on CPU

--- simulations/tracer_only_simulation_run.jl
+++ simulations/tracer_only_simulation_run.jl
@@ -13,24 +13,34 @@ Nt = ConcreteRNumber(3)
 Oceananigans.defaults.FloatType = Float64
 
 @info "Generating model..."
-arch = ReactantState()
-model = GordonBell25.tracer_only_model(arch, Nx=32, Ny=32, Nz=32, Δt=1e-2)
+arch = CPU()
+#arch = ReactantState()
+#arch = Distributed(ReactantState(), partition=Partition(2, 2, 1))
+halo = (0, 0, 0)
+model = GordonBell25.tracer_only_model(arch, halo, Nx=32, Ny=32, Nz=32, Δt=1e-2)
 
 GC.gc(true); GC.gc(false); GC.gc(true)
 
-@info "Compiling..."
-rfirst! = @compile raise=true sync=true first_time_step!(model)
-rstep! = @compile raise=true sync=true time_step!(model)
-rloop! = @compile raise=true sync=true loop!(model, Nt)
+if arch isa CPU
+    first_time_step!(model)
+    time_step!(model)
+    loop!(model, 3)
+else
+    @info "Compiling..."
+    rfirst! = @compile raise=true sync=true first_time_step!(model)
+    rstep! = @compile raise=true sync=true time_step!(model)
+    rloop! = @compile raise=true sync=true loop!(model, Nt)
 
-@info "Running..."
-Reactant.with_profiler("./") do
-    rfirst!(model)
+    @info "Running..."
+    Reactant.with_profiler("./") do
+        rfirst!(model)
+    end
+    Reactant.with_profiler("./") do
+        rstep!(model)
+    end
+    Reactant.with_profiler("./") do
+        rloop!(model, Nt)
+    end
 end
-Reactant.with_profiler("./") do
-    rstep!(model)
-end
-Reactant.with_profiler("./") do
-    rloop!(model, Nt)
-end
+
 @info "Done!"
