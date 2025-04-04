7727e94635aca0e149605fa6bc160e3e46ccd910
Ludovic Raess <ludovic.rass@gmail.com>
2025-04-04 13:41:13 +0200

    Update submission script

--- sharding/alps_scaling_test.jl
+++ sharding/alps_scaling_test.jl
@@ -3,11 +3,11 @@ include("common_submission_generator.jl")
 out_dir = @__DIR__
 
 # run params
-account  = "g191"
-submit   = false
+account  = "c44" #"g191"
+submit   = true
 run_name = "reactant_"
-time     = "00:40:00"
-Ngpus    = [4, 8] #[4, 8, 12, 16]
+time     = "01:00:00"
+Ngpus    = [64, 256] #[4, 8, 12, 16]
 type     = "weak"
 
 gpus_per_node = 4
@@ -35,6 +35,7 @@ function alps_submit_job_writer(cfg::JobConfig, job_name, Nnodes, job_dir, Ngpu,
 #SBATCH --exclusive
 
 export MPICH_GPU_SUPPORT_ENABLED=0
+export JULIA_CUDA_USE_COMPAT=false
 export FI_MR_CACHE_MONITOR=disabled
 
 ulimit -s unlimited
@@ -42,8 +43,12 @@ ulimit -s unlimited
 # Setting `--cpu_bind` is explicitly discouraged:
 # <https://eth-cscs.github.io/cscs-docs/guides/gb2025/#slurm>.
 # We only set it to `verbose` to record what's going on.
-srun --uenv=prgenv-gnu --preserve-env --cpu_bind=verbose \
-    --export=ALL,LD_PRELOAD="/user-environment/linux-sles15-neoverse_v2/gcc-13.3.0/nccl-2.22.3-1-4j6h3ffzysukqpqbvriorrzk2lm762dd/lib/libnccl.so.2" \
+# srun --uenv=prgenv-gnu --preserve-env --cpu_bind=verbose \
+#     --export=ALL,LD_PRELOAD="/user-environment/linux-sles15-neoverse_v2/gcc-13.3.0/nccl-2.22.3-1-4j6h3ffzysukqpqbvriorrzk2lm762dd/lib/libnccl.so.2" \
+#     $(job_dir)/launcher.sh $(Base.julia_cmd()[1]) --project=$(project_path) --startup-file=no --threads=$(cfg.cpus_per_task) -O0 $(run_file)
+
+srun --preserve-env --cpu_bind=verbose \
+    --export=ALL,LD_PRELOAD="/user-environment/linux-sles15-neoverse_v2/gcc-13.2.0/nccl-2.20.3-1-kogp7m7qjex2wykzdnudvs6x3r4dchpn//lib/libnccl.so.2" \
     $(job_dir)/launcher.sh $(Base.julia_cmd()[1]) --project=$(project_path) --startup-file=no --threads=$(cfg.cpus_per_task) -O0 $(run_file)
 """
 end
