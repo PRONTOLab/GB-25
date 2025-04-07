87f7b82709be55b774ed55a64eb7b94466677abb
Simone Silvestri <silvestri.simone0@gmail.com>
2025-04-07 14:57:18 +0200

    kind of working

--- correctness/baroclinic_instability_correctness.jl
+++ correctness/baroclinic_instability_correctness.jl
@@ -2,6 +2,7 @@ using GordonBell25
 using Oceananigans
 using Oceananigans.Units
 using Oceananigans.Architectures: ReactantState
+using Oceananigans.TurbulenceClosures.TKEBasedVerticalDiffusivities: CATKEVerticalDiffusivity
 using Reactant
 
 H = 8 # halo size
@@ -9,18 +10,18 @@ Tx, Ty = 128, 64
 Nx, Ny = (Tx, Ty) .- 2H
 Nz = 32
 
-vitd = VerticallyImplicitTimeDiscretization()
-vertical_diffusivity = VerticalScalarDiffusivity(vitd, κ=1e-5, ν=1e-4)
+# vitd = VerticallyImplicitTimeDiscretization()
+# vertical_diffusivity = VerticalScalarDiffusivity(vitd, κ=1e-5, ν=1e-4)
+vertical_diffusivity = CATKEVerticalDiffusivity()
 
 kw = (
     resolution = 2,
     free_surface = SplitExplicitFreeSurface(substeps=2),
-    # free_surface = ExplicitFreeSurface(),
     coriolis = nothing,
-    buoyancy = nothing, # BuoyancyTracer(),
-    closure = nothing, # vertical_diffusivity,
-    momentum_advection = nothing,
-    tracer_advection = nothing,
+    buoyancy = BuoyancyTracer(),
+    closure = vertical_diffusivity,
+    momentum_advection = WENOVectorInvariant(),
+    tracer_advection = WENO(),
     Δt = 60,
     Nz = 10,
 )
@@ -32,8 +33,12 @@ ui = 1e-3 .* rand(size(vmodel.velocities.u)...)
 vi = 1e-3 .* rand(size(vmodel.velocities.v)...)
 set!(vmodel, u=ui, v=vi)
 
-@show vmodel
-@show rmodel
+@jit Oceananigans.initialize!(rmodel)
+Oceananigans.initialize!(vmodel)
+
+@jit Oceananigans.TimeSteppers.update_state!(rmodel)
+Oceananigans.TimeSteppers.update_state!(vmodel)
+
 
 GordonBell25.sync_states!(rmodel, vmodel)
 GordonBell25.compare_states(rmodel, vmodel)
@@ -44,21 +49,24 @@ rfirst! = @compile sync=true raise=true GordonBell25.first_time_step!(rmodel)
 @time GordonBell25.first_time_step!(vmodel)
 GordonBell25.compare_states(rmodel, vmodel)
 
-
-rfirst! = @compile sync=true raise=true GordonBell25.time_step!(rmodel)
+rstep! = @compile sync=true raise=true GordonBell25.time_step!(rmodel)
 
 for _ in 1:10
-    @time rfirst!(rmodel)
+    @time rstep!(rmodel)
     @time GordonBell25.time_step!(vmodel)
 end
 
-GordonBell25.compare_states(rmodel, vmodel)
+# Everything is kind of correct till here (errors of about 1e-10)
 
+GordonBell25.compare_states(rmodel, vmodel)
 
 Nt = 10
 rNt = ConcreteRNumber(Nt)
 rloop! = @compile sync=true raise=true GordonBell25.loop!(rmodel, rNt)
 @time rloop!(rmodel, rNt)
 @time GordonBell25.loop!(vmodel, Nt)
+
+# Correctness does not work on loops apparently (only for w)
+
 GordonBell25.compare_states(rmodel, vmodel)
 
