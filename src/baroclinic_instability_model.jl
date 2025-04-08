85f868c639a1f63129426273d03b6506e7e1f4cc
Gregory L. Wagner <wagner.greg@gmail.com>
2025-04-07 20:59:35 -0400

    Update baroclinic_instability_model.jl

--- src/baroclinic_instability_model.jl
+++ src/baroclinic_instability_model.jl
@@ -71,13 +71,11 @@ function baroclinic_instability_model(arch, Nx, Ny, Nz; Δt,
 
     Random.seed!(42)
 
-    #=
     if buoyancy isa SeawaterBuoyancy
         set!(model, T=Tᵢ, S=Sᵢ)
     elseif buoyancy isa BuoyancyTracer
         set!(model, b=initial_buoyancy)
     end
-    =#
 
     model.clock.last_Δt = Δt
 
