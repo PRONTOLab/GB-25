96c0747e58fef8597b95a1abba0dd2f9e7a28a1f
William S. Moses <gh@wsmoses.com>
2025-05-15 16:42:18 -0500

    reduced first time step

--- src/timestepping_utils.jl
+++ src/timestepping_utils.jl
@@ -18,9 +18,16 @@ function try_compile_code(f)
     end
 end
 
+function red_first_time_step!(model::AbstractModel, Δt)
+    Oceananigans.TimeSteppers.initialize!(model)
+    # Oceananigans.TimeSteppers.update_state!(model)
+    # Oceananigans.TimeSteppers.time_step!(model, Δt)
+    return nothing
+end
+
 function first_time_step!(model)
     Δt = model.clock.last_Δt
-    Oceananigans.TimeSteppers.first_time_step!(model, Δt)
+    red_first_time_step!(model, Δt)
     return nothing
 end
 
