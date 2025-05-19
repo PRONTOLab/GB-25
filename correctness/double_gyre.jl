4cb7241d9b45774a6e4e802da3b845011f4194a4
jlk9 <josek97@utexas.edu>
2025-05-19 18:52:29 -0500

    Reduced to same inlined time_step for Reactant and CPU architecture

--- correctness/double_gyre.jl
+++ correctness/double_gyre.jl
@@ -141,103 +141,28 @@ end
 
 function bad_time_step!(model, Δt; callbacks=[], euler=false)
 
-    if model.architecture == ReactantState()
-        # Note: Δt cannot change
-        if model.clock.last_Δt isa Reactant.TracedRNumber
-            model.clock.last_Δt.mlir_data = Δt.mlir_data
-        else
-            model.clock.last_Δt = Δt
-        end
+    # If euler, then set χ = -0.5
+    minus_point_five = convert(Float64, -0.5)
+    ab2_timestepper = model.timestepper
+    χ = ifelse(euler, minus_point_five, ab2_timestepper.χ)
+    χ₀ = ab2_timestepper.χ # Save initial value
+    ab2_timestepper.χ = χ
 
-        #=
-        # Be paranoid and update state at iteration 0
-        @trace if model.clock.iteration == 0
-            update_state!(model, callbacks; compute_tendencies=true)
-        end
+    # Full step for tracers, fractional step for velocities.
+    ab2_step!(model, Δt)
 
-        # Take an euler step if:
-        #   * We detect that the time-step size has changed.
-        #   * We detect that this is the "first" time-step, which means we
-        #     need to take an euler step. Note that model.clock.last_Δt is
-        #     initialized as Inf
-        #   * The user has passed euler=true to time_step!
-        @trace if Δt != model.clock.last_Δt
-            euler = true
-        end
-        =#
+    tick!(model.clock, Δt)
+    model.clock.last_Δt = Δt
+    model.clock.last_stage_Δt = Δt # just one stage
 
-        # If euler, then set χ = -0.5
-        minus_point_five = convert(Float64, -0.5)
-        ab2_timestepper = model.timestepper
-        χ = ifelse(euler, minus_point_five, ab2_timestepper.χ)
-        χ₀ = ab2_timestepper.χ # Save initial value
-        ab2_timestepper.χ = χ
+    calculate_pressure_correction!(model, Δt)
+    correct_velocities_and_cache_previous_tendencies!(model, Δt)
 
-        # Full step for tracers, fractional step for velocities.
-        ab2_step!(model, Δt)
+    update_state!(model, callbacks; compute_tendencies=true)
+    step_lagrangian_particles!(model, Δt)
 
-        tick!(model.clock, Δt)
-
-        if model.clock.last_Δt isa Reactant.TracedRNumber
-            model.clock.last_Δt.mlir_data = Δt.mlir_data
-        else
-            model.clock.last_Δt = Δt
-        end
-
-        # just one stage
-        if model.clock.last_stage_Δt isa Reactant.TracedRNumber
-            model.clock.last_stage_Δt.mlir_data = Δt.mlir_data
-        else
-            model.clock.last_stage_Δt = Δt
-        end
-
-        calculate_pressure_correction!(model, Δt)
-        correct_velocities_and_cache_previous_tendencies!(model, Δt)
-
-        update_state!(model, callbacks; compute_tendencies=true)
-        step_lagrangian_particles!(model, Δt)
-
-        # Return χ to initial value
-        ab2_timestepper.χ = χ₀
-
-    elseif model.architecture == CPU()
-        Δt == 0 && @warn "Δt == 0 may cause model blowup!"
-
-        # Be paranoid and update state at iteration 0
-        #model.clock.iteration == 0 && update_state!(model, callbacks; compute_tendencies=true)
-
-        # Take an euler step if:
-        #   * We detect that the time-step size has changed.
-        #   * We detect that this is the "first" time-step, which means we
-        #     need to take an euler step. Note that model.clock.last_Δt is
-        #     initialized as Inf
-        #   * The user has passed euler=true to time_step!
-        euler = euler || (Δt != model.clock.last_Δt)
-        euler && @debug "Taking a forward Euler step."
-
-        # If euler, then set χ = -0.5
-        minus_point_five = convert(eltype(model.grid), -0.5)
-        ab2_timestepper = model.timestepper
-        χ = ifelse(euler, minus_point_five, ab2_timestepper.χ)
-        χ₀ = ab2_timestepper.χ # Save initial value
-        ab2_timestepper.χ = χ
-
-        # Full step for tracers, fractional step for velocities.
-        ab2_step!(model, Δt)
-
-        tick!(model.clock, Δt)
-        model.clock.last_Δt = Δt
-        model.clock.last_stage_Δt = Δt # just one stage
-
-        calculate_pressure_correction!(model, Δt)
-        @apply_regionally correct_velocities_and_cache_previous_tendencies!(model, Δt)
-
-        update_state!(model, callbacks; compute_tendencies=true)
-        step_lagrangian_particles!(model, Δt)
-
-        # Return χ to initial value
-        ab2_timestepper.χ = χ₀
-    end
+    # Return χ to initial value
+    ab2_timestepper.χ = χ₀
 
     return nothing
 end
