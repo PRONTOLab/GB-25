20f8116d1f9576dc832f7977eed871231ee3bd89
jlk9 <josek97@utexas.edu>
2025-05-19 19:29:56 -0500

    Reducing second call of update_state, CTAKE variables are incorrect

--- correctness/double_gyre.jl
+++ correctness/double_gyre.jl
@@ -166,33 +166,13 @@ function time_step_double_gyre!(model, Tᵢ, Sᵢ, wind_stress)
     model.clock.last_Δt = 1200
 
     # Step it forward
-    Δt = model.clock.last_stage_Δt
+    Δt = model.clock.last_Δt
     grid = model.grid
     callbacks = []
 
-    @apply_regionally mask_immersed_model_fields!(model, grid)
-
-    # Update possible FieldTimeSeries used in the model
-    @apply_regionally update_model_field_time_series!(model, model.clock)
-
-    # Update the boundary conditions
-    @apply_regionally update_boundary_condition!(fields(model), model)
-
-    tupled_fill_halo_regions!(prognostic_fields(model), grid, model.clock, fields(model), async=true)
-
-    @apply_regionally replace_horizontal_vector_halos!(model.velocities, model.grid)
-    @apply_regionally compute_auxiliaries!(model)
-
-    fill_halo_regions!(model.diffusivity_fields; only_local_halos = true)
-
-    update_biogeochemical_state!(model.biogeochemistry, model)
-
-    @apply_regionally compute_tendencies!(model, callbacks)
-
-
+    bad_update_state!(model, grid, callbacks)
     ab2_step!(model, Δt)
 
-
     @apply_regionally mask_immersed_model_fields!(model, grid)
 
     # Update possible FieldTimeSeries used in the model
@@ -207,10 +187,6 @@ function time_step_double_gyre!(model, Tᵢ, Sᵢ, wind_stress)
     @apply_regionally compute_auxiliaries!(model)
 
     fill_halo_regions!(model.diffusivity_fields; only_local_halos = true)
-
-    update_biogeochemical_state!(model.biogeochemistry, model)
-
-    @apply_regionally compute_tendencies!(model, callbacks)
 
     return nothing
 end
