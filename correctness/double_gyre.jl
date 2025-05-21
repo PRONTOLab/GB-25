85b2b8d2e284453374769962caccb38d9e20cb1b
jlk9 <josek97@utexas.edu>
2025-05-21 09:10:57 -0500

    Reduced to two calls of compute_auxillaries and one of compute_tendencies

--- correctness/double_gyre.jl
+++ correctness/double_gyre.jl
@@ -128,28 +128,6 @@ using Oceananigans.Fields: tupled_fill_halo_regions!
 using Oceananigans.Models.NonhydrostaticModels: compute_auxiliaries!
 using Oceananigans.Biogeochemistry: update_biogeochemical_state!
 
-function bad_update_state!(model::HydrostaticFreeSurfaceModel, grid, callbacks; compute_tendencies = true)
-
-    @apply_regionally mask_immersed_model_fields!(model, grid)
-
-    # Update possible FieldTimeSeries used in the model
-    @apply_regionally update_model_field_time_series!(model, model.clock)
-
-    # Update the boundary conditions
-    @apply_regionally update_boundary_condition!(fields(model), model)
-
-    tupled_fill_halo_regions!(prognostic_fields(model), grid, model.clock, fields(model), async=true)
-
-    @apply_regionally replace_horizontal_vector_halos!(model.velocities, model.grid)
-    @apply_regionally compute_auxiliaries!(model)
-
-    fill_halo_regions!(model.diffusivity_fields; only_local_halos = true)
-
-    @apply_regionally compute_tendencies!(model, callbacks)
-
-    return nothing
-end
-
 
 function time_step_double_gyre!(model, Tᵢ, Sᵢ, wind_stress)
 
@@ -165,14 +143,6 @@ function time_step_double_gyre!(model, Tᵢ, Sᵢ, wind_stress)
     Δt = model.clock.last_Δt
     grid = model.grid
     callbacks = []
-
-    @apply_regionally mask_immersed_model_fields!(model, grid)
-
-    # Update possible FieldTimeSeries used in the model
-    @apply_regionally update_model_field_time_series!(model, model.clock)
-
-    # Update the boundary conditions
-    @apply_regionally update_boundary_condition!(fields(model), model)
     @apply_regionally compute_auxiliaries!(model)
 
     @apply_regionally compute_tendencies!(model, callbacks)
