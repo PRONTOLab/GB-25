4f931c00f1e3700bbbb9f4938cec8599b965baf7
jlk9 <josek97@utexas.edu>
2025-06-27 11:58:32 -0400

    Updating ClimaOcean

--- correctness/double_gyre.jl
+++ correctness/double_gyre.jl
@@ -128,7 +128,7 @@ end
 function loop!(model)
     Δt = model.clock.last_Δt + 0
     Oceananigans.TimeSteppers.first_time_step!(model, Δt)
-    @trace checkpointing = true track_numbers = false for i = 1:16
+    @trace track_numbers = false for i = 1:16
         Oceananigans.TimeSteppers.time_step!(model, Δt)
     end
     return nothing
