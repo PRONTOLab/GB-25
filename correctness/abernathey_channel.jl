738b6b868c2344881f4553cfbdde7f2640e99a42
jlk9 <josek97@utexas.edu>
2025-09-02 17:04:44 -0500

    Cleaned up script

--- correctness/abernathey_channel.jl
+++ correctness/abernathey_channel.jl
@@ -53,62 +53,34 @@ end
 =#
 Oceananigans.defaults.FloatType = Float64
 
-const Lx = 1000kilometers # zonal domain length [m]
-const Ly = 2000kilometers # meridional domain length [m]
-
-# Architecture
-architecture = ReactantState() #GPU()
+#
+# Model parameters to set first:
+#
 
 # number of grid points
 Nx = 48 #96  # LowRes: 48
 Ny = 96 #192 # LowRes: 96
 Nz = 32
 
-halo_size = 4 #3 for non-immersed grid
-
 # stretched grid
 k_center = collect(1:Nz)
 Δz_center = @. 10 * 1.104^(Nz - k_center)
 
+const Lx = 1000kilometers # zonal domain length [m]
+const Ly = 2000kilometers # meridional domain length [m]
 const Lz = sum(Δz_center)
 
-z_faces = vcat([-Lz], -Lz .+ cumsum(Δz_center))
-z_faces[Nz+1] = 0
+# Coriolis variables:
+const f = -1e-4
+const β = 1e-11
 
-underlying_grid = RectilinearGrid(architecture,
-    topology = (Periodic, Bounded, Bounded),
-    size = (Nx, Ny, Nz),
-    halo = (halo_size, halo_size, halo_size),
-    x = (0, Lx),
-    y = (0, Ly),
-    z = (-Lz, 0))
+halo_size = 4 #3 for non-immersed grid
 
-
-# full ridge function:
-function ridge_function(x, y)
-    zonal = (Lz+100)exp(-(x - Lx/2)^2/(1e6kilometers))
-    gap   = 1 - 0.5(tanh((y - (Ly/6))/1e5) - tanh((y - (Ly/2))/1e5))
-    return zonal * gap - Lz
-end
-
-# Make into a ridge array:
-ridge = Field{Center, Center, Nothing}(underlying_grid)
-set!(ridge, ridge_function)
-
-grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(ridge))
-
-@info "Built a grid."
-
-#@allowscalar @show grid
-
-#####
-##### Boundary conditions
-#####
-
-α = 2e-4     # [K⁻¹] thermal expansion coefficient
-g = 9.8061   # [m/s²] gravitational constant
-cᵖ = 3994.0   # [J/K]  heat capacity
-ρ = 999.8    # [kg/m³] reference density
+# Other model parameters:
+const α = 2e-4     # [K⁻¹] thermal expansion coefficient
+const g = 9.8061   # [m/s²] gravitational constant
+const cᵖ = 3994.0   # [J/K]  heat capacity
+const ρ = 999.8    # [kg/m³] reference density
 
 parameters = (
     Ly = Ly,
@@ -124,119 +96,157 @@ parameters = (
     λt = 7.0days                         # relaxation time scale [s]
 )
 
-@inline function buoyancy_flux(i, j, grid, clock, model_fields, p)
-    y = ynode(j, grid, Center())
-    return ifelse(y < p.y_shutoff, p.Qᵇ * cos(3π * y / p.Ly), 0.0)
+# full ridge function:
+function ridge_function(x, y)
+    zonal = (Lz+100)exp(-(x - Lx/2)^2/(1e6kilometers))
+    gap   = 1 - 0.5(tanh((y - (Ly/6))/1e5) - tanh((y - (Ly/2))/1e5))
+    return zonal * gap - Lz
 end
 
-buoyancy_flux_bc = FluxBoundaryCondition(buoyancy_flux, discrete_form = true, parameters = parameters)
+function make_grid(architecture, Nx, Ny, Nz, Δz_center)
+    z_faces = vcat([-Lz], -Lz .+ cumsum(Δz_center))
+    z_faces[Nz+1] = 0
 
-u_stress_bc = FluxBoundaryCondition(Field{Face, Center, Nothing}(grid))
+    underlying_grid = RectilinearGrid(architecture,
+        topology = (Periodic, Bounded, Bounded),
+        size = (Nx, Ny, Nz),
+        halo = (halo_size, halo_size, halo_size),
+        x = (0, Lx),
+        y = (0, Ly),
+        z = (-Lz, 0))
 
-@inline u_drag(i, j, grid, clock, model_fields, p) = @inbounds -p.μ * p.Lz * model_fields.u[i, j, 1]
-@inline v_drag(i, j, grid, clock, model_fields, p) = @inbounds -p.μ * p.Lz * model_fields.v[i, j, 1]
+    # Make into a ridge array:
+    ridge = Field{Center, Center, Nothing}(underlying_grid)
+    set!(ridge, ridge_function)
 
-u_drag_bc = FluxBoundaryCondition(u_drag, discrete_form = true, parameters = parameters)
-v_drag_bc = FluxBoundaryCondition(v_drag, discrete_form = true, parameters = parameters)
-
-b_bcs = FieldBoundaryConditions(top = buoyancy_flux_bc)
-
-u_bcs = FieldBoundaryConditions(top = u_stress_bc, bottom = u_drag_bc)
-v_bcs = FieldBoundaryConditions(bottom = v_drag_bc)
-
-#####
-##### Coriolis
-#####
-
-const f = -1e-4
-const β = 1e-11
-coriolis = BetaPlane(f₀ = f, β = β)
-
-#####
-##### Forcing and initial condition
-#####
-
-@inline initial_buoyancy(z, p) = p.ΔB * (exp(z / p.h) - exp(-p.Lz / p.h)) / (1 - exp(-p.Lz / p.h))
-@inline mask(y, p) = max(0.0, y - p.y_sponge) / (Ly - p.y_sponge)
-
-
-@inline function buoyancy_relaxation(i, j, k, grid, clock, model_fields, p)
-    timescale = p.λt
-    y = ynode(j, grid, Center())
-    z = znode(k, grid, Center())
-    target_b = initial_buoyancy(z, p)
-    b = @inbounds model_fields.b[i, j, k]
-
-    return -1 / timescale * mask(y, p) * (b - target_b)
+    grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(ridge))
+    return grid
 end
 
-Fb = Forcing(buoyancy_relaxation, discrete_form = true, parameters = parameters)
-
-# closure
-
-κh = 0.5e-5 # [m²/s] horizontal diffusivity
-νh = 30.0   # [m²/s] horizontal viscocity
-κz = 0.5e-5 # [m²/s] vertical diffusivity
-νz = 3e-4   # [m²/s] vertical viscocity
-
-horizontal_closure = HorizontalScalarDiffusivity(ν = νh, κ = κh)
-vertical_closure = VerticalScalarDiffusivity(ν = νz, κ = κz)
-
-vertical_closure_CATKE = CATKEVerticalDiffusivity(minimum_tke=1e-7,
-                                                  maximum_tracer_diffusivity=0.3,
-                                                  maximum_viscosity=0.5)
-
-
 #####
-##### Model building
+##### Model construction:
 #####
 
-@info "Building a model..."
+function build_model(grid, Δt₀, parameters)
 
-model = HydrostaticFreeSurfaceModel(
-    grid = grid,
-    free_surface = SplitExplicitFreeSurface(substeps=500),
-    momentum_advection = WENO(),
-    tracer_advection = WENO(),
-    buoyancy = BuoyancyTracer(),
-    coriolis = coriolis,
-    closure = (horizontal_closure, vertical_closure, vertical_closure_CATKE),
-    tracers = (:b, :e),
-    boundary_conditions = (b = b_bcs, u = u_bcs, v = v_bcs),
-    forcing = (; b = Fb)
-)
+    @inline function buoyancy_flux(i, j, grid, clock, model_fields, p)
+        y = ynode(j, grid, Center())
+        return ifelse(y < p.y_shutoff, p.Qᵇ * cos(3π * y / p.Ly), 0.0)
+    end
 
-@info "Built $model."
+    buoyancy_flux_bc = FluxBoundaryCondition(buoyancy_flux, discrete_form = true, parameters = parameters)
+
+    u_stress_bc = FluxBoundaryCondition(Field{Face, Center, Nothing}(grid))
+
+    @inline u_drag(i, j, grid, clock, model_fields, p) = @inbounds -p.μ * p.Lz * model_fields.u[i, j, 1]
+    @inline v_drag(i, j, grid, clock, model_fields, p) = @inbounds -p.μ * p.Lz * model_fields.v[i, j, 1]
+
+    u_drag_bc = FluxBoundaryCondition(u_drag, discrete_form = true, parameters = parameters)
+    v_drag_bc = FluxBoundaryCondition(v_drag, discrete_form = true, parameters = parameters)
+
+    b_bcs = FieldBoundaryConditions(top = buoyancy_flux_bc)
+
+    u_bcs = FieldBoundaryConditions(top = u_stress_bc, bottom = u_drag_bc)
+    v_bcs = FieldBoundaryConditions(bottom = v_drag_bc)
+
+    #####
+    ##### Coriolis
+    #####
+    coriolis = BetaPlane(f₀ = f, β = β)
+
+    #####
+    ##### Forcing and initial condition
+    #####
+
+    @inline initial_buoyancy(z, p) = p.ΔB * (exp(z / p.h) - exp(-p.Lz / p.h)) / (1 - exp(-p.Lz / p.h))
+    @inline mask(y, p) = max(0.0, y - p.y_sponge) / (Ly - p.y_sponge)
+
+
+    @inline function buoyancy_relaxation(i, j, k, grid, clock, model_fields, p)
+        timescale = p.λt
+        y = ynode(j, grid, Center())
+        z = znode(k, grid, Center())
+        target_b = initial_buoyancy(z, p)
+        b = @inbounds model_fields.b[i, j, k]
+
+        return -1 / timescale * mask(y, p) * (b - target_b)
+    end
+
+    Fb = Forcing(buoyancy_relaxation, discrete_form = true, parameters = parameters)
+
+    # closure
+
+    κh = 0.5e-5 # [m²/s] horizontal diffusivity
+    νh = 30.0   # [m²/s] horizontal viscocity
+    κz = 0.5e-5 # [m²/s] vertical diffusivity
+    νz = 3e-4   # [m²/s] vertical viscocity
+
+    horizontal_closure = HorizontalScalarDiffusivity(ν = νh, κ = κh)
+    vertical_closure = VerticalScalarDiffusivity(ν = νz, κ = κz)
+
+    vertical_closure_CATKE = CATKEVerticalDiffusivity(minimum_tke=1e-7,
+                                                    maximum_tracer_diffusivity=0.3,
+                                                    maximum_viscosity=0.5)
+
+    @info "Building a model..."
+
+    model = HydrostaticFreeSurfaceModel(
+        grid = grid,
+        free_surface = SplitExplicitFreeSurface(substeps=500),
+        momentum_advection = WENO(),
+        tracer_advection = WENO(),
+        buoyancy = BuoyancyTracer(),
+        coriolis = coriolis,
+        closure = (horizontal_closure, vertical_closure, vertical_closure_CATKE),
+        tracers = (:b, :e),
+        boundary_conditions = (b = b_bcs, u = u_bcs, v = v_bcs),
+        forcing = (; b = Fb)
+    )
+
+    model.clock.last_Δt = Δt₀
+
+    return model
+end
 
 #####
-##### Initial conditions
+##### Special initial and boundary conditions
 #####
 
 # wind stress:
 function wind_stress_init(grid, p)
-    wind_stress = Field{Face, Center, Nothing}(grid)
-
     @inline u_stress(x, y) = -p.τ * sin(π * y / p.Ly)
-
+    wind_stress = Field{Face, Center, Nothing}(grid)
     set!(wind_stress, u_stress)
     return wind_stress
 end
 
-wind_stress = wind_stress_init(model.grid, parameters)
-
 # resting initial condition
-ε(σ) = σ * randn()
-bᵢ_function(x, y, z) = parameters.ΔB * (exp(z / parameters.h) - exp(-Lz / parameters.h)) / (1 - exp(-Lz / parameters.h)) + ε(1e-8)
-bᵢ = Field{Center, Center, Center}(grid)
-set!(bᵢ, bᵢ_function)
+function buoyancy_init(grid, parameters)
+    ε(σ) = σ * randn()
+    bᵢ_function(x, y, z) = parameters.ΔB * (exp(z / parameters.h) - exp(-Lz / parameters.h)) / (1 - exp(-Lz / parameters.h)) + ε(1e-8)
+    bᵢ = Field{Center, Center, Center}(grid)
+    set!(bᵢ, bᵢ_function)
+    return bᵢ
+end
+
+# Architecture
+architecture = ReactantState() #GPU()
+
+# Timestep size:
+Δt₀ = 5minutes 
+
+# Make the grid:
+grid        = make_grid(architecture, Nx, Ny, Nz, Δz_center)
+model       = build_model(grid, Δt₀, parameters)
+wind_stress = wind_stress_init(model.grid, parameters)
+bᵢ          = buoyancy_init(model.grid, parameters)
+
+
+@info "Built $model."
 
 #####
-##### Simulation building
+##### Forward simulation (not actually using the Simulation struct)
 #####
-Δt₀ = 5minutes
-stop_iteration = 10
-
-model.clock.last_Δt = Δt₀
 
 function loop!(model)
     Δt = model.clock.last_Δt
@@ -265,18 +275,18 @@ function estimate_tracer_error(model, initial_buoyancy, wind_stress)
     run_reentrant_channel_model!(model, initial_buoyancy, wind_stress)
     # Compute the mean mixed layer depth:
     #compute!(mld)
-    Nλ, Nφ, Nz = size(model.grid)
+    Nx, Ny, Nz = size(model.grid)
     #=
     avg_mld = 0.0
     
-    for j0 = 1:Nφ, i0 = 1:Nλ
+    for j0 = 1:Nx, i0 = 1:Ny
         @allowscalar avg_mld += @inbounds model.velocities.u[i0, j0, 1]^2
     end
-    avg_mld = avg_mld / (Nλ * Nφ)
+    avg_mld = avg_mld / (Nx * Ny)
     =#
     # Hard way
     c² = parent(model.tracers.b).^2
-    avg_mld = sum(c²) / (Nλ * Nφ * Nz)
+    avg_mld = sum(c²) / (Nx * Ny * Nz)
     return avg_mld
 end
 
@@ -315,12 +325,13 @@ filename        = graph_directory * "data_init.jld2"
 
 if !isdir(graph_directory) Base.Filesystem.mkdir(graph_directory) end
 
-if grid == underlying_grid
-    bottom_height = Field{Center, Center, Nothing}(underlying_grid)
+if grid == model.grid.underlying_grid
+    bottom_height = Field{Center, Center, Nothing}(model.grid.underlying_grid)
     set!(bottom_height, -Lz)
 else
     bottom_height = interior(model.grid.immersed_boundary.bottom_height)
 end
+
 
 jldsave(filename; Nx, Ny, Nz,
                   bottom_height=convert(Array, bottom_height),
@@ -345,3 +356,4 @@ jldsave(filename; Nx, Ny, Nz,
                   u=convert(Array, interior(model.velocities.u)),
                   v=convert(Array, interior(model.velocities.v)),
                   w=convert(Array, interior(model.velocities.w)))
+
