38569ef28eda1b51da4bbcc74d1531b728a4f9fc
jlk9 <josek97@utexas.edu>
2025-09-08 11:19:44 -0500

    Current linear EOS setup

--- correctness/reentrant_channel_plotting.jl
+++ correctness/reentrant_channel_plotting.jl
@@ -6,7 +6,7 @@ using GLMakie
 #
 
 
-graph_directory = "run_abernathy_model_1000steps_noRidge_v98/"
+graph_directory = "run_abernathy_model_1000steps_noRidge_linearEOS/"
 
 data = jldopen(graph_directory * "data_init.jld2", "r")
 
