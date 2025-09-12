a71d6db960d2579cfcaf4a81ebd3c34570d5e57b
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-09-13 00:59:10 +0100

    Fix instantiation of Center in boundary conditions

--- sharding/sharded_baroclinic_instability_simulation_run.jl
+++ sharding/sharded_baroclinic_instability_simulation_run.jl
@@ -113,7 +113,7 @@ if test_type == "fill_all_halos"
 
 elseif test_type == "fill_north_south"
 
-    c = Oceananigans.Fields.Center()
+    c = Oceananigans.Fields.Center
     loc = (c, c, c)
     north_south_bcs = FieldBoundaryConditions(model.grid, loc, west=nothing, east=nothing, top=nothing, bottom=nothing)
     north_south_field = CenterField(model.grid; boundary_conditions=north_south_bcs)
@@ -132,7 +132,7 @@ elseif test_type == "fill_north_south"
 
 elseif test_type == "fill_east_west"
 
-    c = Oceananigans.Fields.Center()
+    c = Oceananigans.Fields.Center
     loc = (c, c, c)
     east_west_bcs = FieldBoundaryConditions(model.grid, loc, south=nothing, north=nothing, top=nothing, bottom=nothing)
     east_west_field = CenterField(model.grid; boundary_conditions=east_west_bcs)
