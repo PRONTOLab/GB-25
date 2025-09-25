f6a9ee65dbb1ce4062cbc2dcb3fda61314e24c2d
jlk9 <josek97@utexas.edu>
2025-09-25 15:32:41 -0500

    add checkpointing

--- correctness/abernathey_channel.jl
+++ correctness/abernathey_channel.jl
@@ -267,7 +267,7 @@ end
 
 function loop!(model)
     Δt = model.clock.last_Δt
-    @trace mincut = true track_numbers = false for i = 1:1000
+    @trace mincut = true checkpointing = true track_numbers = false for i = 1:1000
         time_step!(model, Δt)
     end
     return nothing
