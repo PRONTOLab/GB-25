a42eb9abf11dcb45342c92da720b68b17d47654c
jlk9 <josek97@utexas.edu>
2025-12-19 08:53:43 -0600

    Reduced to one kernel launch

--- correctness/correctness_sharded_baroclinic_instability_simulation_run.jl
+++ correctness/correctness_sharded_baroclinic_instability_simulation_run.jl
@@ -68,56 +68,18 @@ using Oceananigans.BoundaryConditions: fill_halo_regions!, permute_boundary_cond
                                        fill_west_and_east_halo!, fill_south_and_north_halo!, fill_bottom_and_top_halo!,
                                        fill_halo_size, fill_halo_offset, parent_size_and_offset, fill_periodic_west_and_east_halo!
 
-@show @which fields(rmodel)
-@show @which fields(vmodel)
-
-function my_tupled_fill_halo_regions!(fields, grid, args...; kwargs...)
-
-    not_reduced_fields = my_fill_reduced_field_halos!(fields, args...; kwargs)
-    
-    if !isempty(not_reduced_fields) # ie not reduced, and with default_indices
-
-        my_fill_halo_regions!(map(data, not_reduced_fields),
-                           map(instantiated_location, not_reduced_fields),
-                           grid, args...; kwargs...)
-    end
-    
-    return nothing
-end
-
-function my_fill_reduced_field_halos!(fields, args...; kwargs)
-
-    @show fields
-
-    not_reduced_fields = Field[]
-    for f in fields
-        bcs = boundary_conditions(f)
-        if !isnothing(bcs)
-            if f isa ReducedField || !(f isa FullField)
-                # Windowed and reduced fields
-                #fill_halo_regions!(f, args...; kwargs...)
-                @info "We got a ReducedField"
-            else
-                push!(not_reduced_fields, f)
-                @info "We did NOT get a ReducedField"
-            end
-        end
-    end
-
-    return tuple(not_reduced_fields...)
-end
 
 "Fill halo regions in ``x``, ``y``, and ``z`` for a given field's data."
-function my_fill_halo_regions!(c, loc, grid, args...;
+function my_fill_halo_regions!(c, grid;
                             fill_boundary_normal_velocities = true, kwargs...)
     arch = architecture(grid)
 
-    my_fill_halo_event!(c, loc, arch, grid, args...; kwargs...)
+    my_fill_halo_event!(c, arch, grid; kwargs...)
 
     return nothing
 end
 
-function my_fill_halo_event!(c, loc, arch, grid, args...;
+function my_fill_halo_event!(c, arch, grid;
                           async = false, # This kwargs is specific to DistributedGrids, here is does nothing
                           kwargs...)
 
@@ -127,12 +89,12 @@ function my_fill_halo_event!(c, loc, arch, grid, args...;
     size   = :yz
     offset = (0, 0)
 
-    my_fill_west_and_east_halo!(c, offset, loc, arch, grid, args...; kwargs...)
+    my_fill_west_and_east_halo!(c, offset, arch, grid; kwargs...)
 
     return nothing
 end
 
-function my_fill_west_and_east_halo!(c, offset, loc, arch, grid, args...; only_local_halos = false, kw...)
+function my_fill_west_and_east_halo!(c, offset, arch, grid; only_local_halos = false, kw...)
 
     c_parent = parent.(c)
     yz_size  = (minimum([size(t, 2) for t in c_parent]), minimum([size(t, 3) for t in c_parent]))
@@ -142,58 +104,10 @@ function my_fill_west_and_east_halo!(c, offset, loc, arch, grid, args...; only_l
     return nothing
 end
 
-@inline my_prognostic_fields(model) = (u=model.velocities.u, v=model.velocities.v, T=model.tracers.T, S=model.tracers.S)
+@inline my_prognostic_fields(model) = (model.velocities.u.data, model.velocities.v.data, model.tracers.T.data, model.tracers.S.data)
 
-my_tupled_fill_halo_regions!(my_prognostic_fields(vmodel), vmodel.grid, vmodel.clock, fields(vmodel), async=true)
-@jit my_tupled_fill_halo_regions!(my_prognostic_fields(rmodel), rmodel.grid, rmodel.clock, fields(rmodel), async=true)
+my_fill_halo_regions!(my_prognostic_fields(vmodel), vmodel.grid, async=true)
+@jit my_fill_halo_regions!(my_prognostic_fields(rmodel), rmodel.grid, async=true)
 
 @info "After initialization and update state:"
 GordonBell25.compare_states(rmodel, vmodel; include_halos, throw_error, rtol, atol)
-
-#=
-
-GordonBell25.sync_states!(rmodel, vmodel)
-rfirst! = @compile sync=true raise=true GordonBell25.first_time_step!(rmodel)
-@showtime rfirst!(rmodel)
-@showtime GordonBell25.first_time_step!(vmodel)
-
-@info "After first time step:"
-GordonBell25.compare_states(rmodel, vmodel; include_halos, throw_error, rtol, atol)
-
-rstep! = @compile sync=true raise=true GordonBell25.time_step!(rmodel)
-
-@info "Warm up:"
-@showtime rstep!(rmodel)
-@showtime rstep!(rmodel)
-@showtime GordonBell25.time_step!(vmodel)
-@showtime GordonBell25.time_step!(vmodel)
-
-Nt = 10
-@info "Time step with Reactant:"
-for _ in 1:Nt
-    @showtime rstep!(rmodel)
-end
-
-@info "Time step vanilla:"
-for _ in 1:Nt
-    @showtime GordonBell25.time_step!(vmodel)
-end
-
-@info "After $(Nt) steps:"
-GordonBell25.compare_states(rmodel, vmodel; include_halos, throw_error, rtol, atol)
-
-GordonBell25.sync_states!(rmodel, vmodel)
-@jit Oceananigans.TimeSteppers.update_state!(rmodel)
-
-@info "After syncing and updating state again:"
-GordonBell25.compare_states(rmodel, vmodel; include_halos, throw_error, rtol, atol)
-
-Nt = 100
-rNt = ConcreteRNumber(Nt)
-rloop! = @compile sync=true raise=true GordonBell25.loop!(rmodel, rNt)
-@showtime rloop!(rmodel, rNt)
-@showtime GordonBell25.loop!(vmodel, Nt)
-
-@info "After a loop of $(Nt) steps:"
-GordonBell25.compare_states(rmodel, vmodel; include_halos, throw_error, rtol, atol)
-=#
