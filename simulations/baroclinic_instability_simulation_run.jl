da79d79a3be616ead139a80023d97afe253d326c
jlk9 <josek97@utexas.edu>
2026-01-13 20:01:07 -0600

    Inlined hydrostatic_free_surface_tracer_tendency

--- simulations/baroclinic_instability_simulation_run.jl
+++ simulations/baroclinic_instability_simulation_run.jl
@@ -20,6 +20,7 @@ model = baroclinic_instability_model(arch, resolution=8, Δt=60, Nz=10)
 GC.gc(true); GC.gc(false); GC.gc(true)
 
 using InteractiveUtils
+using KernelAbstractions: @kernel, @index
 
 using Oceananigans: initialize!
 using Oceananigans.Utils: launch!, _launch!, configure_kernel, work_layout, mapped_kernel
@@ -31,6 +32,22 @@ using Oceananigans.ImmersedBoundaries: get_active_cells_map
 using Oceananigans.Models: interior_tendency_kernel_parameters
 using Oceananigans.Models.HydrostaticFreeSurfaceModels: compute_hydrostatic_free_surface_tendency_contributions!, compute_hydrostatic_momentum_tendencies!,
                                                         compute_hydrostatic_free_surface_Gc!
+
+
+using Oceananigans.BuoyancyFormulations
+using Oceananigans.Coriolis
+using Oceananigans.Operators
+using Oceananigans.Operators: ∂xᶠᶜᶜ, ∂yᶜᶠᶜ
+using Oceananigans.TurbulenceClosures: ∂ⱼ_τ₁ⱼ, ∂ⱼ_τ₂ⱼ, ∇_dot_qᶜ
+using Oceananigans.Biogeochemistry: biogeochemical_transition, biogeochemical_drift_velocity, biogeochemical_auxiliary_fields
+using Oceananigans.TurbulenceClosures: immersed_∂ⱼ_τ₁ⱼ, immersed_∂ⱼ_τ₂ⱼ, immersed_∂ⱼ_τ₃ⱼ, immersed_∇_dot_qᶜ
+using Oceananigans.Advection: div_Uc, U_dot_∇u, U_dot_∇v
+using Oceananigans.Forcings: with_advective_forcing
+using Oceananigans.TurbulenceClosures: shear_production, buoyancy_flux, dissipation, closure_turbulent_velocity
+using Oceananigans.Utils: sum_of_velocities
+using KernelAbstractions: @private
+
+import Oceananigans.TurbulenceClosures: hydrostatic_turbulent_kinetic_energy_tendency
 
 function my_compute_tendencies!(model)
 
@@ -71,7 +88,7 @@ function my_compute_hydrostatic_free_surface_tendency_contributions!(model, kern
                     c_forcing)
 
     _my_launch!(arch, grid, kernel_parameters,
-            compute_hydrostatic_free_surface_Gc!,
+            my_compute_hydrostatic_free_surface_Gc!,
             c_tendency,
             grid,
             args)
@@ -81,29 +98,62 @@ end
 
 @inline function _my_launch!(arch, grid, workspec, kernel!, first_kernel_arg, other_kernel_args...)
 
-    location = Oceananigans.location(first_kernel_arg)
-
-    loop!, worksize = my_configure_kernel(arch, grid, workspec, kernel!)
-
-    # Don't launch kernels with no size
-    haswork = true
-
-    if haswork
-        loop!(first_kernel_arg, other_kernel_args...)
-    end
-
-    return nothing
-end
-
-@inline function my_configure_kernel(arch, grid, workspec, kernel!)
-
     workgroup = (16, 16)
     worksize  = (48, 24, 10)
 
     dev  = Oceananigans.Architectures.device(arch)
-    loop = kernel!(dev, workgroup, worksize)
+    loop! = kernel!(dev, workgroup, worksize)
 
-    return loop, worksize
+    # Don't launch kernels with no size
+    loop!(first_kernel_arg, other_kernel_args...)
+
+    return nothing
+end
+
+""" Calculate the right-hand-side of the tracer advection-diffusion equation. """
+@kernel function my_compute_hydrostatic_free_surface_Gc!(Gc, grid, args)
+    i, j, k = @index(Global, NTuple)
+    @inbounds Gc[i, j, k] = my_hydrostatic_free_surface_tracer_tendency(i, j, k, grid, args...)
+end
+
+@inline free_surface_fields(free_surface) = (; η=free_surface.η)
+@inline hydrostatic_fields(velocities, free_surface, tracers) =
+    merge((u=velocities.u, v=velocities.v, w=velocities.w),
+          tracers,
+          free_surface_fields(free_surface))
+
+@inline function my_hydrostatic_free_surface_tracer_tendency(i, j, k, grid,
+                                                          val_tracer_index::Val{tracer_index},
+                                                          val_tracer_name,
+                                                          advection,
+                                                          closure,
+                                                          c_immersed_bc,
+                                                          buoyancy,
+                                                          biogeochemistry,
+                                                          velocities,
+                                                          free_surface,
+                                                          tracers,
+                                                          diffusivities,
+                                                          auxiliary_fields,
+                                                          clock,
+                                                          forcing) where tracer_index
+
+    @inbounds c = tracers[tracer_index]
+    model_fields = merge(hydrostatic_fields(velocities, free_surface, tracers),
+                         auxiliary_fields,
+                         biogeochemical_auxiliary_fields(biogeochemistry))
+
+    biogeochemical_velocities = biogeochemical_drift_velocity(biogeochemistry, val_tracer_name)
+    closure_velocities = closure_turbulent_velocity(closure, diffusivities, val_tracer_name)
+
+    total_velocities = sum_of_velocities(velocities, biogeochemical_velocities, closure_velocities)
+    total_velocities = with_advective_forcing(forcing, total_velocities)
+
+    return ( - div_Uc(i, j, k, grid, advection, total_velocities, c)
+             - ∇_dot_qᶜ(i, j, k, grid, closure, diffusivities, val_tracer_index, c, clock, model_fields, buoyancy)
+             - immersed_∇_dot_qᶜ(i, j, k, grid, c, c_immersed_bc, closure, diffusivities, val_tracer_index, clock, model_fields)
+             + biogeochemical_transition(i, j, k, grid, biogeochemistry, val_tracer_name, clock, model_fields)
+             + forcing(i, j, k, grid, clock, model_fields))
 end
 
 @info "Compiling..."
