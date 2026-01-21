32709302eceaedce2e5c54d17f7a8158cbc4e468
jlk9 <josek97@utexas.edu>
2026-01-20 21:19:32 -0600

    Inlined maybe_extend_halos

--- simulations/ocean_climate_simulation_run.jl
+++ simulations/ocean_climate_simulation_run.jl
@@ -3,6 +3,8 @@ using GordonBell25: data_free_ocean_climate_model_init, gaussian_islands_tripola
 using Oceananigans.Architectures: ReactantState
 using Reactant
 
+using InteractiveUtils
+
 using ClimaOcean: ocean_simulation
 
 using Oceananigans
@@ -21,11 +23,17 @@ using Oceananigans.Models.HydrostaticFreeSurfaceModels: constructor_field_names,
 using Oceananigans.TurbulenceClosures: add_closure_specific_boundary_conditions
 using Oceananigans.TimeSteppers: Clock
 
+using Oceananigans.Models.HydrostaticFreeSurfaceModels.SplitExplicitFreeSurfaces: maybe_extend_halos
+
+using Oceananigans.Grids: topology, halo_size, LeftConnected, RightConnected, FullyConnected, with_halo
+
 # Reactant.Compiler.SROA_ATTRIBUTOR[] = false
 
 preamble()
 
 Ninner = ConcreteRNumber(3)
+
+const ConnectedTopology = Union{LeftConnected, RightConnected, FullyConnected}
 
 function my_data_free_ocean_climate_model_init(
     arch=ReactantState();
@@ -39,40 +47,53 @@ function my_data_free_ocean_climate_model_init(
     free_surface = SplitExplicitFreeSurface(substeps=30)
     tracers = (:T, :S)
 
-    # We need boundary_conditions:
-    embedded_boundary_conditions = merge(extract_boundary_conditions(nothing),
-                                         extract_boundary_conditions(tracers),
-                                         extract_boundary_conditions(nothing),
-                                         extract_boundary_conditions(nothing))
-
     # Next, we form a list of default boundary conditions:
     field_names = constructor_field_names(nothing, tracers, free_surface, NamedTuple(), nothing, grid)
-    default_boundary_conditions = NamedTuple{field_names}(FieldBoundaryConditions() for name in field_names)
+    boundary_conditions = NamedTuple{field_names}(FieldBoundaryConditions() for name in field_names)
 
     # Then we merge specified, embedded, and default boundary conditions. Specified boundary conditions
     # have precedence, followed by embedded, followed by default.
-    boundary_conditions = merge(default_boundary_conditions, embedded_boundary_conditions)
+    @show @which regularize_field_boundary_conditions(boundary_conditions, grid, field_names)
     boundary_conditions = regularize_field_boundary_conditions(boundary_conditions, grid, field_names)
 
-    # Finally, we ensure that closure-specific boundary conditions, such as
-    # those required by CATKEVerticalDiffusivity, are enforced:
-    boundary_conditions = add_closure_specific_boundary_conditions(nothing,
-                                                                   boundary_conditions,
-                                                                   grid,
-                                                                   tracernames(tracers),
-                                                                   nothing)
+    @show boundary_conditions
 
     # We need velocities:
+    @show @which hydrostatic_velocity_fields(nothing, grid, Clock(grid), boundary_conditions)
     velocities = hydrostatic_velocity_fields(nothing, grid, Clock(grid), boundary_conditions)
 
     # Problem line:
-    @allowscalar free_surface = materialize_free_surface(free_surface, velocities, grid)
-
-    #@allowscalar ocean = HydrostaticFreeSurfaceModel(; grid,
-    #                                                    tracers,
-    #                                                    free_surface)
+    @show @which materialize_free_surface(free_surface, velocities, grid)
+    @allowscalar free_surface = my_materialize_free_surface(free_surface, velocities, grid)
 
     return ocean
+end
+
+function my_materialize_free_surface(free_surface, velocities, grid)
+
+    TX, TY, _   = topology(grid)
+    substepping = free_surface.substepping
+
+    @show @which maybe_extend_halos(TX, TY, grid, substepping)
+    maybe_extended_grid = my_maybe_extend_halos(TX, TY, grid, substepping)
+
+end
+
+function my_maybe_extend_halos(TX, TY, grid, substepping)
+
+    old_halos = halo_size(grid)
+    Nsubsteps = length(substepping.averaging_weights)
+
+    Hx = TX() isa ConnectedTopology ? max(Nsubsteps+2, old_halos[1]) : old_halos[1]
+    Hy = TY() isa ConnectedTopology ? max(Nsubsteps+2, old_halos[2]) : old_halos[2]
+
+    new_halos = (Hx, Hy, old_halos[3])
+
+    if new_halos == old_halos
+        return grid
+    else
+        return with_halo(new_halos, grid)
+    end
 end
 
 @info "Generating model..."
