49263b77d552e2f63eaac278cb0fd925a1d77050
jlk9 <josek97@utexas.edu>
2026-01-21 14:27:05 -0600

    Reduced to parent assignment

--- simulations/ocean_climate_simulation_run.jl
+++ simulations/ocean_climate_simulation_run.jl
@@ -16,8 +16,8 @@ using ClimaOcean.Oceans: default_ocean_closure, TwoColorRadiation, default_or_ov
 default_gravitational_acceleration = Oceananigans.defaults.gravitational_acceleration
 default_planet_rotation_rate = Oceananigans.defaults.planet_rotation_rate
 
-using Oceananigans.Fields: tracernames
-using Oceananigans.BoundaryConditions: DefaultBoundaryCondition, FieldBoundaryConditions, regularize_field_boundary_conditions
+using Oceananigans.Fields: tracernames, set_to_field!
+using Oceananigans.BoundaryConditions: DefaultBoundaryCondition, FieldBoundaryConditions, regularize_field_boundary_conditions, fill_halo_regions!
 using Oceananigans.Models: extract_boundary_conditions
 using Oceananigans.Models.HydrostaticFreeSurfaceModels: constructor_field_names, hydrostatic_velocity_fields, materialize_free_surface
 using Oceananigans.TurbulenceClosures: add_closure_specific_boundary_conditions
@@ -27,7 +27,7 @@ using Oceananigans.Models.HydrostaticFreeSurfaceModels.SplitExplicitFreeSurfaces
 
 using Oceananigans.Grids: topology, halo_size, LeftConnected, RightConnected, FullyConnected, with_halo
 
-using Oceananigans.ImmersedBoundaries: has_active_cells_map, has_active_z_columns, materialize_immersed_boundary
+using Oceananigans.ImmersedBoundaries: has_active_cells_map, has_active_z_columns, materialize_immersed_boundary, compute_numerical_bottom_height!, GridFittedBottom
 
 # Reactant.Compiler.SROA_ATTRIBUTOR[] = false
 
@@ -37,8 +37,7 @@ Ninner = ConcreteRNumber(3)
 
 const ConnectedTopology = Union{LeftConnected, RightConnected, FullyConnected}
 
-function my_data_free_ocean_climate_model_init(
-    arch=ReactantState();
+function my_data_free_ocean_climate_model_init(arch;
     # Horizontal resolution
     resolution::Real = 2, # 1/4 for quarter degree
     # Vertical resolution
@@ -48,9 +47,9 @@ function my_data_free_ocean_climate_model_init(
     grid = gaussian_islands_tripolar_grid(arch, resolution, Nz)
 
     # Problem line:
-    @allowscalar free_surface = my_materialize_free_surface(grid)
+    #@allowscalar free_surface = my_materialize_free_surface(grid)
 
-    return ocean
+    return grid
 end
 
 function my_materialize_free_surface(grid)
@@ -68,9 +67,31 @@ end
 function my_with_halo(halo, ibg)
     underlying_grid = with_halo(halo, ibg.underlying_grid)
 
-    @show @which materialize_immersed_boundary(underlying_grid, ibg.immersed_boundary)
-    materialized_ib = materialize_immersed_boundary(underlying_grid, ibg.immersed_boundary)
+    return underlying_grid
+end
+
+function my_materialize_immersed_boundary(grid, ib)
+    bottom_field = Field{Center, Center, Nothing}(grid)
+
+    @show @which parent(bottom_field.data)
+    @show typeof(parent(bottom_field.data))
+    @show size(parent(bottom_field.data))
+    @show typeof(bottom_field.data)
+    @show size(bottom_field.data)
+
+    @show @which parent(ib.bottom_height.data)
+    @show typeof(parent(ib.bottom_height.data))
+    @show size(parent(ib.bottom_height.data))
+    @show typeof(ib.bottom_height.data)
+    @show size(ib.bottom_height.data)
+
+    parent(bottom_field.data) .= parent(ib.bottom_height.data)
+    return new_ib
 end
 
 @info "Generating model..."
-model = my_data_free_ocean_climate_model_init(ReactantState())
+grid = my_data_free_ocean_climate_model_init(ReactantState())
+
+@allowscalar underlying_grid = my_materialize_free_surface(grid)
+
+materialized_ib = my_materialize_immersed_boundary(underlying_grid, grid.immersed_boundary)
