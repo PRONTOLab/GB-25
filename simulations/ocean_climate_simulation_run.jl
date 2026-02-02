966b4ff815acd9fc73f0505e52b1c8e742bffbb7
jlk9 <josek97@utexas.edu>
2026-02-02 12:06:54 -0600

    It's atmosphere interpolation

--- simulations/ocean_climate_simulation_run.jl
+++ simulations/ocean_climate_simulation_run.jl
@@ -10,6 +10,8 @@ using Oceananigans
 using Oceananigans: initialize!
 using Oceananigans.TimeSteppers: update_state!, time_step!
 
+using ClimaOcean.OceanSeaIceModels: interpolate_state!
+
 # Reactant.Compiler.SROA_ATTRIBUTOR[] = false
 
 preamble()
@@ -17,25 +19,20 @@ preamble()
 Ninner = ConcreteRNumber(3)
 
 @info "Generating model..."
-model = data_free_ocean_climate_model_init(ReactantState(); resolution=4, Nz=10)
+coupled_model = data_free_ocean_climate_model_init(ReactantState(); resolution=4, Nz=10)
 Ninner = ConcreteRNumber(2)
 
 GC.gc(true); GC.gc(false); GC.gc(true)
 
-Δt = model.clock.last_Δt
-
-function my_first_time_step!(model, Δt)
-    initialize!(model)
-    # The first update_state is conditionally gated from within time_step! normally, but not Reactant
-    update_state!(model)
-    time_step!(model, Δt)
-    return nothing
-end
 
 @info "Compiling..."
-@show @which initialize!(model)
-@show @which update_state!(model)
-@show @which time_step!(model)
-rfirst! = @compile raise=true sync=true my_first_time_step!(model, Δt)
+
+atmosphere = coupled_model.atmosphere
+exchanger  = coupled_model.interfaces.exchanger
+grid       = exchanger.grid
+
+@show @which interpolate_state!(exchanger.atmosphere, grid, atmosphere, coupled_model)
+
+rfirst! = @compile raise=true sync=true interpolate_state!(exchanger.atmosphere, grid, atmosphere, coupled_model)
 
 @info "Done!"
