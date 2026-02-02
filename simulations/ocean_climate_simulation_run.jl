7d0b42d5c9f0b07ed244f48c0248a85d82e01ab4
jlk9 <josek97@utexas.edu>
2026-02-02 12:51:33 -0600

    Inlined interpolate_state

--- simulations/ocean_climate_simulation_run.jl
+++ simulations/ocean_climate_simulation_run.jl
@@ -11,6 +11,11 @@ using Oceananigans: initialize!
 using Oceananigans.TimeSteppers: update_state!, time_step!
 
 using ClimaOcean.OceanSeaIceModels: interpolate_state!
+using ClimaOcean.Atmospheres: _interpolate_primary_atmospheric_state!
+using ClimaOcean.OceanSeaIceModels.InterfaceComputations: interface_kernel_parameters
+
+using Oceananigans.OutputReaders: TimeInterpolator
+using Oceananigans.Utils: launch!
 
 # Reactant.Compiler.SROA_ATTRIBUTOR[] = false
 
@@ -24,6 +29,77 @@ Ninner = ConcreteRNumber(2)
 
 GC.gc(true); GC.gc(false); GC.gc(true)
 
+function my_interpolate_state!(exchanger, grid, atmosphere, clock)
+    atmosphere_grid = atmosphere.grid
+
+    # Basic model properties
+    arch = grid.architecture
+
+    #####
+    ##### First interpolate atmosphere time series
+    ##### in time and to the ocean grid.
+    #####
+
+    # We use .data here to save parameter space (unlike Field, adapt_structure for
+    # fts = FieldTimeSeries does not return fts.data)
+    atmosphere_velocities = (u = atmosphere.velocities.u.data,
+                             v = atmosphere.velocities.v.data)
+
+    atmosphere_tracers = (T = atmosphere.tracers.T.data,
+                          q = atmosphere.tracers.q.data)
+
+    Qs = atmosphere.downwelling_radiation.shortwave
+    Qℓ = atmosphere.downwelling_radiation.longwave
+    downwelling_radiation = (shortwave=Qs.data, longwave=Qℓ.data)
+    freshwater_flux = map(ϕ -> ϕ.data, atmosphere.freshwater_flux)
+    atmosphere_pressure = atmosphere.pressure.data
+
+    # Extract info for time-interpolation
+    u = atmosphere.velocities.u # for example
+    atmosphere_times = u.times
+    atmosphere_backend = u.backend
+    atmosphere_time_indexing = u.time_indexing
+
+    atmosphere_fields = exchanger.state
+    space_fractional_indices = exchanger.regridder
+
+    # Simplify NamedTuple to reduce parameter space consumption.
+    # See https://github.com/CliMA/ClimaOcean.jl/issues/116.
+    atmosphere_data = (u = atmosphere_fields.u.data,
+                       v = atmosphere_fields.v.data,
+                       T = atmosphere_fields.T.data,
+                       p = atmosphere_fields.p.data,
+                       q = atmosphere_fields.q.data,
+                       Qs = atmosphere_fields.Qs.data,
+                       Qℓ = atmosphere_fields.Qℓ.data,
+                       Mp = atmosphere_fields.Mp.data)
+
+    kernel_parameters = interface_kernel_parameters(grid)
+
+    # Assumption, should be generalized
+    ua = atmosphere.velocities.u
+
+    times = ua.times
+    time_indexing = ua.time_indexing
+    t = clock.time
+    time_interpolator = TimeInterpolator(ua.time_indexing, times, clock.time)
+
+    launch!(arch, grid, kernel_parameters,
+            _interpolate_primary_atmospheric_state!,
+            atmosphere_data,
+            space_fractional_indices,
+            time_interpolator,
+            grid,
+            atmosphere_velocities,
+            atmosphere_tracers,
+            atmosphere_pressure,
+            downwelling_radiation,
+            freshwater_flux,
+            atmosphere_backend,
+            atmosphere_time_indexing)
+
+end
+
 
 @info "Compiling..."
 
@@ -31,8 +107,6 @@ atmosphere = coupled_model.atmosphere
 exchanger  = coupled_model.interfaces.exchanger
 grid       = exchanger.grid
 
-@show @which interpolate_state!(exchanger.atmosphere, grid, atmosphere, coupled_model)
-
-rfirst! = @compile raise=true sync=true interpolate_state!(exchanger.atmosphere, grid, atmosphere, coupled_model)
+rfirst! = @compile raise=true sync=true my_interpolate_state!(exchanger.atmosphere, grid, atmosphere, coupled_model.clock)
 
 @info "Done!"
