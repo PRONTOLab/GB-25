437c986de3c624651ef2087ce3cf6a3ec8e09560
William S. Moses <gh@wsmoses.com>
2026-02-01 21:09:09 -0600

    Stashing red comm

--- src/timestepping_utils.jl
+++ src/timestepping_utils.jl
@@ -20,20 +20,81 @@ end
 
 function first_time_step!(model)
     Δt = model.clock.last_Δt
-    Oceananigans.TimeSteppers.first_time_step!(model, Δt)
+    # Oceananigans.TimeSteppers.first_time_step!(model, Δt)
     return nothing
 end
 
 function time_step!(model)
     Δt = model.clock.last_Δt + 0
-    Oceananigans.TimeSteppers.time_step!(model, Δt)
+    # Oceananigans.TimeSteppers.time_step!(model, Δt)
     return nothing
 end
 
+using Oceananigans.TimeSteppers:
+    update_state!,
+    tick!,
+    calculate_pressure_correction!,
+    correct_velocities_and_cache_previous_tendencies!,
+    step_lagrangian_particles!,
+    QuasiAdamsBashforth2TimeStepper
+
+using Oceananigans.Models.HydrostaticFreeSurfaceModels:
+    step_free_surface!,
+    local_ab2_step!,
+    compute_free_surface_tendency!
+
+
+using Oceananigans.Architectures
+using Oceananigans.BoundaryConditions
+
+using Oceananigans: UpdateStateCallsite
+using Oceananigans.Biogeochemistry: update_biogeochemical_state!
+using Oceananigans.BoundaryConditions: update_boundary_condition!, replace_horizontal_vector_halos!
+using Oceananigans.TurbulenceClosures: compute_diffusivities!
+using Oceananigans.ImmersedBoundaries: mask_immersed_field!, mask_immersed_field_xy!, inactive_node
+using Oceananigans.Models: update_model_field_time_series!
+using Oceananigans.Models.NonhydrostaticModels: update_hydrostatic_pressure!, p_kernel_parameters
+using Oceananigans.Fields: tupled_fill_halo_regions!
+
+import Oceananigans.Models.NonhydrostaticModels: compute_auxiliaries!
+import Oceananigans.TimeSteppers: update_state!
+
+using Oceananigans.Biogeochemistry: update_tendencies!
+
+function myupdate!(model)
+    grid = model.grid
+
+    tupled_fill_halo_regions!(prognostic_fields(model), grid, model.clock, fields(model), async=true)
+
+    # @apply_regionally 
+    arch = architecture(grid)
+
+    # Calculate contributions to momentum and tracer tendencies from fluxes and volume terms in the
+    # interior of the domain. The active cells map restricts the computation to the active cells in the
+    # interior if the grid is _immersed_ and the `active_cells_map` kwarg is active
+    active_cells_map = get_active_cells_map(model.grid, Val(:interior))
+    kernel_parameters = interior_tendency_kernel_parameters(arch, grid)
+
+    Oceananigans.Models.HydrostaticFreeSurfaceModels.compute_hydrostatic_free_surface_tendency_contributions!(model, kernel_parameters; active_cells_map)
+
+    Oceananigans.Models.complete_communication_and_compute_buffer!(model, grid, arch)
+
+    # Calculate contributions to momentum and tracer tendencies from user-prescribed fluxes across the
+    # boundaries of the domain
+    Oceananigans.Models.HydrostaticFreeSurfaceModels.compute_hydrostatic_boundary_tendency_contributions!(model.timestepper.Gⁿ,
+                                                         model.architecture,
+                                                         model.velocities,
+                                                         model.tracers,
+                                                         model.clock,
+                                                         fields(model),
+                                                         model.closure,
+                                                         model.buoyancy)
+
+end
+
 function loop!(model, Ninner)
-    Δt = model.clock.last_Δt + 0
     @trace track_numbers=false for _ = 1:Ninner
-        Oceananigans.TimeSteppers.time_step!(model, Δt)
+	myupdate!(model)
     end
     return nothing
 end
