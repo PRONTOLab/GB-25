2a217d0874748644b7e948a68da3ea21fd3e76ae
jlk9 <josek97@utexas.edu>
2026-02-02 17:22:07 -0600

    Inlined intrinsic_vector

--- simulations/ocean_climate_simulation_run.jl
+++ simulations/ocean_climate_simulation_run.jl
@@ -17,7 +17,7 @@ using ClimaOcean.OceanSeaIceModels.InterfaceComputations: interface_kernel_param
 using Oceananigans.OutputReaders: TimeInterpolator
 using Oceananigans.Fields: FractionalIndices
 using Oceananigans.Utils: launch!, _launch!
-using Oceananigans.Operators: intrinsic_vector
+using Oceananigans.Operators: intrinsic_vector, getvalue, rotation_angle
 
 using KernelAbstractions: @kernel, @index
 
@@ -39,58 +39,37 @@ function my_interpolate_state!(exchanger, grid, atmosphere, clock)
     # Basic model properties
     arch = grid.architecture
 
-    #####
-    ##### First interpolate atmosphere time series
-    ##### in time and to the ocean grid.
-    #####
-
-    # We use .data here to save parameter space (unlike Field, adapt_structure for
-    # fts = FieldTimeSeries does not return fts.data)
-    atmosphere_velocities = (u = atmosphere.velocities.u.data,
-                             v = atmosphere.velocities.v.data)
-
-    # Extract info for time-interpolation
-    u = atmosphere.velocities.u # for example
-    atmosphere_times = u.times
-    atmosphere_backend = u.backend
-    atmosphere_time_indexing = u.time_indexing
-
-    space_fractional_indices = exchanger.regridder
-
     kernel_parameters = interface_kernel_parameters(grid)
-
-    # Assumption, should be generalized
-    ua = atmosphere.velocities.u
-
-    times = ua.times
-    time_indexing = ua.time_indexing
-    t = clock.time
-    time_interpolator = TimeInterpolator(ua.time_indexing, times, clock.time)
 
     _launch!(arch, grid, kernel_parameters,
             _my_interpolate_primary_atmospheric_state!,
-            space_fractional_indices,
-            time_interpolator,
-            grid,
-            atmosphere_velocities,
-            atmosphere_backend,
-            atmosphere_time_indexing)
+            grid)
 
 end
 
-@kernel function _my_interpolate_primary_atmospheric_state!(space_fractional_indices,
-                                                         time_interpolator,
-                                                         exchange_grid,
-                                                         atmos_velocities,
-                                                         atmos_backend,
-                                                         atmos_time_indexing)
+@kernel function _my_interpolate_primary_atmospheric_state!(exchange_grid)
 
     i, j = @index(Global, NTuple)
 
     # Convert atmosphere velocities (usually defined on a latitude-longitude grid) to
     # the frame of reference of the native grid
     kᴺ = size(exchange_grid, 3) # index of the top ocean cell
-    uₐ, vₐ = intrinsic_vector(i, j, kᴺ, exchange_grid, 0.0, 0.0)
+    uₐ, vₐ = my_intrinsic_vector(i, j, kᴺ, exchange_grid.underlying_grid, 0.0, 0.0)
+end
+
+@inline function my_intrinsic_vector(i, j, k, grid, uₑ, vₑ)
+
+    u = getvalue(uₑ, i, j, k, grid)
+    v = getvalue(vₑ, i, j, k, grid)
+
+    θ_degrees = rotation_angle(i, j, grid)
+    sinθ = sind(θ_degrees)
+    cosθ = cosd(θ_degrees)
+
+    uᵢ = u * cosθ - v * sinθ
+    vᵢ = u * sinθ + v * cosθ
+
+    return uᵢ, vᵢ
 end
 
 
@@ -100,29 +79,7 @@ atmosphere = coupled_model.atmosphere
 exchanger  = coupled_model.interfaces.exchanger
 grid       = exchanger.grid
 
-time_interpolator = TimeInterpolator(atmosphere.velocities.u.time_indexing, atmosphere.velocities.u.times, coupled_model.clock.time)
-
-u = atmosphere.velocities.u # for example
-atmosphere_backend = u.backend
-atmosphere_time_indexing = u.time_indexing
-
-space_fractional_indices = exchanger.atmosphere.regridder
-
-@allowscalar fi = space_fractional_indices.i[1, 1, 1]
-@allowscalar fj = space_fractional_indices.j[1, 1, 1]
-
-x_itp = FractionalIndices(fi, fj, nothing)
-t_itp = time_interpolator
-atmos_args = (x_itp, t_itp, atmosphere_backend, atmosphere_time_indexing)
-
-@show @which interp_atmos_time_series(atmosphere.velocities.u.data, atmos_args...)
-@show @which interp_atmos_time_series(atmosphere.velocities.v.data, atmos_args...)
-
-@allowscalar uₐ = interp_atmos_time_series(atmosphere.velocities.u.data, atmos_args...)
-@allowscalar vₐ = interp_atmos_time_series(atmosphere.velocities.v.data, atmos_args...)
-
-@allowscalar @show uₐ
-@allowscalar @show vₐ
+@show @which intrinsic_vector(1, 1, size(grid, 3), grid.underlying_grid, 0.0, 0.0)
 
 rfirst! = @compile raise=true sync=true my_interpolate_state!(exchanger.atmosphere, grid, atmosphere, coupled_model.clock)
 
