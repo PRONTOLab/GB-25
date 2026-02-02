c284e65650a780f6eacbca3051d201ce6cac25c8
jlk9 <josek97@utexas.edu>
2026-02-01 22:16:47 -0600

    Current state of MLIR compile error

--- simulations/ocean_climate_simulation_run.jl
+++ simulations/ocean_climate_simulation_run.jl
@@ -3,6 +3,13 @@ using GordonBell25: data_free_ocean_climate_model_init
 using Oceananigans.Architectures: ReactantState
 using Reactant
 
+using InteractiveUtils
+
+using Oceananigans
+
+using Oceananigans: initialize!
+using Oceananigans.TimeSteppers: update_state!, time_step!
+
 # Reactant.Compiler.SROA_ATTRIBUTOR[] = false
 
 preamble()
@@ -10,24 +17,25 @@ preamble()
 Ninner = ConcreteRNumber(3)
 
 @info "Generating model..."
-model = data_free_ocean_climate_model_init(ReactantState())
+model = data_free_ocean_climate_model_init(ReactantState(); resolution=4, Nz=10)
 Ninner = ConcreteRNumber(2)
 
 GC.gc(true); GC.gc(false); GC.gc(true)
 
-@info "Compiling..."
-rfirst! = @compile raise=true sync=true first_time_step!(model)
-rstep! = @compile raise=true sync=true time_step!(model)
-rloop! = @compile raise=true sync=true loop!(model, Ninner)
+Δt = model.clock.last_Δt
 
-@info "Running..."
-Reactant.with_profiler("./") do
-    rfirst!(model)
+function my_first_time_step!(model, Δt)
+    initialize!(model)
+    # The first update_state is conditionally gated from within time_step! normally, but not Reactant
+    update_state!(model)
+    time_step!(model, Δt)
+    return nothing
 end
-Reactant.with_profiler("./") do
-    rstep!(model)
-end
-Reactant.with_profiler("./") do
-    rloop!(model, Ninner)
-end
+
+@info "Compiling..."
+@show @which initialize!(model)
+@show @which update_state!(model)
+@show @which time_step!(model)
+rfirst! = @compile raise=true sync=true my_first_time_step!(model, Δt)
+
 @info "Done!"
