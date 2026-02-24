13fcefec32d9c07878995abfc2cf7d36fdaae423
jlk9 <josek97@utexas.edu>
2026-02-24 11:37:17 +0000

    Reducing for Reactant 226 error

--- correctness/abernathey_channel.jl
+++ correctness/abernathey_channel.jl
@@ -18,12 +18,12 @@ using Oceananigans.TurbulenceClosures: CATKEVerticalDiffusivity, HorizontalFormu
 
 using SeawaterPolynomials
 
-using ClimaOcean.Diagnostics: MixedLayerDepthField
-
 using Reactant
-using GordonBell25
+using CUDA
 using Oceananigans.Architectures: ReactantState
 #Reactant.set_default_backend("cpu")
+
+Reactant.MLIR.IR.DUMP_MLIR_ALWAYS[] = true
 
 using Enzyme
 
@@ -39,9 +39,6 @@ using Oceananigans.Models.HydrostaticFreeSurfaceModels: compute_hydrostatic_free
 const Ntimesteps = 400
 
 Oceananigans.defaults.FloatType = Float64
-
-graph_directory = "run_abernathy_model_ad_spinup1000_100steps_test/"
-#graph_directory = "run_abernathy_model_ad_spinup40000000_8100steps/"
 
 #
 # Model parameters to set first:
@@ -69,46 +66,7 @@ z_faces[Nz+1] = 0
 
 Δz = reshape(Δz, 1, :)
 
-# Coriolis variables:
-const f = -1e-4
-const β = 1e-11
-
 halo_size = 4 #3 for non-immersed grid
-
-# Other model parameters:
-const α = 2e-4     # [K⁻¹] thermal expansion coefficient
-const g = 9.8061   # [m/s²] gravitational constant
-const cᵖ = 3994.0   # [J/K]  heat capacity
-const ρ = 999.8    # [kg/m³] reference density
-
-parameters = (
-    Ly = Ly,
-    Lz = Lz,
-    Qᵇ = 10 / (ρ * cᵖ) * α * g,            # buoyancy flux magnitude [m² s⁻³]
-    Qᵀ = 10 / (ρ * cᵖ),                    # temperature flux magnitude
-    y_shutoff = 5 / 6 * Ly,                # shutoff location for buoyancy flux [m]
-    τ = 0.2 / ρ,                           # surface kinematic wind stress [m² s⁻²]
-    μ = 1 / 30days,                      # bottom drag damping time-scale [s⁻¹]
-    ΔB = 8 * α * g,                      # surface vertical buoyancy gradient [s⁻²]
-    ΔT = 8,                              # surface vertical temperature gradient
-    H = Lz,                              # domain depth [m]
-    h = 1000.0,                          # exponential decay scale of stable stratification [m]
-    y_sponge = 19 / 20 * Ly,               # southern boundary of sponge layer [m]
-    λt = 7.0days                         # relaxation time scale [s]
-)
-
-# full ridge function:
-function ridge_function(x, y)
-    zonal = (Lz+3000)exp(-(x - Lx/2)^2/(1e6kilometers))
-    gap   = 1 - 0.5(tanh((y - (Ly/6))/1e5) - tanh((y - (Ly/2))/1e5))
-    return zonal * gap - Lz
-end
-
-function wall_function(x, y)
-    zonal = (x > 470kilometers) && (x < 530kilometers)
-    gap   = (y < 400kilometers) || (y > 1000kilometers)
-    return (Lz+1) * zonal * gap - Lz
-end
 
 
 function make_grid(architecture, Nx, Ny, Nz, z_faces)
@@ -121,32 +79,34 @@ function make_grid(architecture, Nx, Ny, Nz, z_faces)
         y = (0, Ly),
         z = z_faces)
 
-    # Make into a ridge array:
-    ridge = Field{Center, Center, Nothing}(underlying_grid)
-    set!(ridge, wall_function)
-
-    grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(ridge))
-    return grid
+    return underlying_grid
 end
 
 #####
 ##### Model construction:
 #####
 
-function build_model(grid, Δt₀, parameters)
+function build_model(grid)
 
     @info "Building a model..."
+
+    @show @which HydrostaticFreeSurfaceModel(
+        grid = grid,
+        free_surface = nothing,
+        momentum_advection = nothing,
+        tracer_advection = nothing,
+        buoyancy = nothing,
+        tracers = nothing
+    )
 
     model = HydrostaticFreeSurfaceModel(
         grid = grid,
         free_surface = nothing,
-        momentum_advection = WENO(order=3),
-        tracer_advection = WENO(order=3),
+        momentum_advection = nothing,
+        tracer_advection = nothing,
         buoyancy = nothing,
-        tracers = (:T, :S, :e)
+        tracers = nothing
     )
-
-    model.clock.last_Δt = Δt₀
 
     return model
 end
@@ -155,36 +115,11 @@ end
 ##### Forward simulation (not actually using the Simulation struct)
 #####
 
-function loop!(model)
-    Δt = model.clock.last_Δt
-    @trace mincut = true checkpointing = true track_numbers = false for i = 1:Ntimesteps
-        time_step!(model, Δt)
-        my_compute_tendencies!(model, [])
-    end
-    return nothing
-end
-
-function run_reentrant_channel_model!(model)
-
-    # Initialize the model
-    model.clock.iteration = 0
-    model.clock.time = 0
-
-    # Step it forward
-    loop!(model)
-
-    return nothing
-end
 
 function estimate_tracer_error(model)
-    run_reentrant_channel_model!(model)
-    
-    Nx, Ny, Nz = size(model.grid)
+    model.clock.iteration = 0
 
-    # Compute the zonal transport:
-    zonal_transport = (model.velocities.u[x_midpoint,1:Ny,1:Nz] .* model.grid.Δyᵃᶜᵃ)
-
-    return sum(zonal_transport) / 1e6 # Put it in Sverdrups
+    return nothing
 end
 
 function differentiate_tracer_error(model, dmodel)
@@ -196,20 +131,9 @@ function differentiate_tracer_error(model, dmodel)
     return dedν
 end
 
-function my_compute_tendencies!(model, callbacks)
-
-    grid = model.grid
-    arch = grid.architecture
-
-    # Calculate contributions to momentum and tracer tendencies from fluxes and volume terms in the
-    # interior of the domain. The active cells map restricts the computation to the active cells in the
-    # interior if the grid is _immersed_ and the `active_cells_map` kwarg is active
-    #active_cells_map = get_active_cells_map(model.grid, Val(:interior))
-    #kernel_parameters = interior_tendency_kernel_parameters(arch, grid)
-
-    #compute_hydrostatic_free_surface_tendency_contributions!(model, kernel_parameters; active_cells_map)
-
-    return nothing
+mutable struct MyModel{G, T}
+    grid::G  
+    clock::Clock{T}
 end
 
 #####
@@ -219,50 +143,17 @@ end
 # Architecture
 architecture = ReactantState()
 
-# Timestep size:
-Δt₀ = 2.5minutes 
-
 # Make the grid:
-grid          = make_grid(architecture, Nx, Ny, Nz, z_faces)
-model         = build_model(grid, Δt₀, parameters)
+grid   = make_grid(architecture, Nx, Ny, Nz, z_faces)
+model  = build_model(grid)
+dmodel = Enzyme.make_zero(model)
 
-@info "Built $model."
-
-dmodel         = Enzyme.make_zero(model)
-
-# Trying zonal transport:
+mymodel = MyModel(model.grid, model.clock)
+dmymodel = Enzyme.make_zero(mymodel)
 
 @info "Compiling the model run..."
 tic = time()
-restimate_tracer_error = @compile raise_first=true raise=true sync=true  estimate_tracer_error(model)
 rdifferentiate_tracer_error = @compile raise_first=true raise=true sync=true  differentiate_tracer_error(model, dmodel)
 compile_toc = time() - tic
 
 @show compile_toc
-
-@info "Running the simulation..."
-
-# Spinup the model for a sufficient amount of time, save the T and S from this state:
-tic = time()
-restimate_tracer_error(model)
-spinup_toc_first = time() - tic
-@show spinup_toc_first
-
-tic = time()
-restimate_tracer_error(model)
-spinup_toc_second = time() - tic
-@show spinup_toc_second
-
-tic = time()
-dedν = rdifferentiate_tracer_error(model, dmodel)
-run_toc_first = time() - tic
-@show run_toc_first
-
-tic = time()
-dedν = rdifferentiate_tracer_error(model, dmodel)
-run_toc_second = time() - tic
-@show run_toc_second
-
-
-@show @which compute_tendencies!(model, [])
-            
