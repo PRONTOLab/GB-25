d110d555a9b479beb793e2c0f1d1e13cc19b7318
Mosè Giordano <765740+giordano@users.noreply.github.com>
2026-03-18 21:00:22 +0000

    Update sharded_baroclinic_instability_simulation_run.jl

--- sharding/sharded_baroclinic_instability_simulation_run.jl
+++ sharding/sharded_baroclinic_instability_simulation_run.jl
@@ -29,14 +29,15 @@ ENV["JULIA_DEBUG"] = "Reactant_jll,Reactant"
 using GordonBell25
 using GordonBell25: first_time_step!, time_step!, loop!, factors, is_distributed_env_present
 using Oceananigans
-if parsed_args["float-type"] ∈ ("Float64", "f64")
-    Oceananigans.defaults.FloatType = Float64
+
+Oceananigans.defaults.FloatType = if parsed_args["float-type"] ∈ ("Float64", "f64")
+    Float64
 elseif parsed_args["float-type"] ∈ ("Float32", "f32")
-    Oceananigans.defaults.FloatType = Float32
+    Float32
 elseif parse_args["float-type"] ∈ ("Float16", "f16")
-    Oceananigans.defaults.FloatType = Float16
+    Float16
 elseif parse_args["float-type"] ∈ ("BFloat16", "bf16")
-    Oceananigans.defaults.FloatType = Core.BFloat16
+    Core.BFloat16
 else
     throw(AssertionError("Unknown float type $(parsed_args["float-type"])"))
 end
