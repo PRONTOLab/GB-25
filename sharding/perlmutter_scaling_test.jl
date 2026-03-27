2bca9b38b41b1ffcf31ae4f6e711768033fbfac8
romanlee <romanlee@lbl.gov>
2026-03-26 18:47:46 -0700

    Remove old nccl hacks

--- sharding/perlmutter_scaling_test.jl
+++ sharding/perlmutter_scaling_test.jl
@@ -40,24 +40,20 @@ function perlmutter_submit_job_writer(cfg::JobConfig, job_name, Nnodes, job_dir,
 
 source /global/common/software/nersc9/julia/scripts/activate_beta.sh
 ml load julia/1.11.7
-# this nccl is too old
-# ml load nccl/2.24.3
-module load nccl/2.29.2-cu13
 
-export LD_PRELOAD=/usr/lib64/libstdc++.so.6
+module load nccl/2.29.2-cu13
 
 export SBATCH_ACCOUNT=$(cfg.account)
 export SALLOC_ACCOUNT=$(cfg.account)
 export JULIA_CUDA_MEMORY_POOL=none
 
-# Equivalent to \$NCCL_DIR/lib/libnccl.so but will also work if module doesn't set NCCL_DIR
-# export NCCL_LIB_PATH=\$(julia -e "n=\\"libnccl\\";using Libdl;dlopen(n);filter(contains(n),dllist())|>first|>println")
-
 #
 # HACKS to get this to work on Perlmutter
 #
 
-# export LD_PRELOAD=\$NCCL_LIB_PATH
+# fixes incompatability btwn system julia and nccl
+export LD_PRELOAD=/usr/lib64/libstdc++.so.6
+
 export FI_CXI_RDZV_GET_MIN=0
 export FI_CXI_SAFE_DEVMEM_COPY_THRESHOLD=16777216
 # export MPICH_SMP_SINGLE_COPY_MODE=NONE
