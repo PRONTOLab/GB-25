8ebbd728b76dfda11d2fe5809c4517e733e5e345
Billy Moses <gh@wsmoses.com>
2026-04-06 03:23:16 +0000

    fix

--- correctness/correctness_sharded_baroclinic_instability_simulation_run.jl
+++ correctness/correctness_sharded_baroclinic_instability_simulation_run.jl
@@ -72,6 +72,8 @@ using Oceananigans.Fields: tupled_fill_halo_regions!
 import Oceananigans.Models.NonhydrostaticModels: compute_auxiliaries!
 import Oceananigans.TimeSteppers: update_state!
 
+import Oceananigans.Models.HydrostaticFreeSurfaceModels: displacement
+
 # Mask immersed fields
 function mask_immersed_model_fields!(model, grid)
     η = displacement(model.free_surface)
