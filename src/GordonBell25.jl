1e0ae5b72b9685aaf2f7d2a037d7cb61d9a6f60d
dkytezab <danielkytezable@gmail.com>
2026-04-06 17:36:43 -0700

    Add sharding + saving utils

--- src/GordonBell25.jl
+++ src/GordonBell25.jl
@@ -3,6 +3,8 @@ module GordonBell25
 export first_time_step!, time_step!, loop!, try_code_hlo
 export parse_baroclinic_instability_args, float_type_from_args, multifloat_from_args
 export moist_baroclinic_wave_model, set_moist_baroclinic_wave!
+export local_shards_to_host, save_sharded_fields, save_model_state
+export load_global_field, load_checkpoint_metadata, extract_model_fields
 
 include("arg_parsing.jl")
 include("model_utils.jl")
@@ -11,6 +13,7 @@ include("data_free_ocean_climate_model.jl")
 include("baroclinic_instability_model.jl")
 include("moist_baroclinic_wave_model.jl")
 include("sharding_utils.jl")
+include("sharded_io.jl")
 include("correctness.jl")
 include("precompile.jl")
 
